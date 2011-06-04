2a7ad949e479af230c98c375bd626d7228fb2475
Henrique Vicente <henriquevicente@gmail.com>
2011-06-04 18:10:34 -0300

    ML_Dom modified, now with two methods that helps in leading with multiple values creation.
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- application/models/ML/Dom.php
+++ application/models/ML/Dom.php
@@ -17,5 +17,37 @@ class ML_Dom extends DOMDocument
 		$element->appendChild($root_text);
 		
 		return $element;
-	}	
+	}
+	
+	public function newTextAttributes($element, $array_of_attributes)
+	{
+	    foreach($array_of_attributes as $key => $value)
+	    {
+	        $element->appendChild($this->NewTextAttribute($key, $value));
+	    }
+	}
+	
+	public function newTextElements($element, $array_of_elements)
+	{
+	    foreach($array_of_elements as $key => $value)
+	    {
+	        $element->appendChild($this->NewTextElement($key, $value));
+	    }
+	}
+	
+    public function newMultipleTextElements($root_element, $element_name, $array_of_multiple_elements) {
+		foreach($array_of_multiple_elements as $array_of_element)
+		{
+			$element = $root_element->appendChild($this->newTextElement($element_name, null));
+			$this->newTextElements($element, $array_of_element);
+		}
+	}
+	
+	public function newMultipleTextAttributes($root_element, $element_name, $array_of_multiple_attributes) {
+		foreach($array_of_multiple_attributes as $array_of_attributes)
+		{
+			$element = $root_element->appendChild($this->newTextElement($element_name, null));
+			$this->newTextAttributes($element, $array_of_attributes);
+		}
+	}
 }
