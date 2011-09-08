93dc2b9bddacd69418f9d2445a3eb98afcf551ad
Henrique Vicente <henriquevicente@gmail.com>
2011-09-07 22:58:58 -0300

    some places now using filter_input of $_GET to read external values
    it's still incomplete, but this will be consistency by the weekend at most.
    
    Signed-off-by: Henrique Vicente de Oliveira Pinto <henvic@henvic-mp.local>

--- application/controllers/ProgressController.php
+++ application/controllers/ProgressController.php
@@ -4,8 +4,8 @@ class ProgressController extends Zend_Controller_Action
     public function uploadAction ()
     {
         header("Content-Type: application/json");
-        if (isset($_GET['push'])) //not using push by now
-        {
+        
+        if(!is_null(filter_input(INPUT_GET, "push", FILTER_UNSAFE_RAW))) {
             $adapter = new Zend_ProgressBar_Adapter_JsPush(
             array('updateMethodName' => 'Zend_ProgressBar_Update', 
             'finishMethodName' => 'Zend_ProgressBar_Finish'));
