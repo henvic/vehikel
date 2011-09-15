5eb7cacf879b91aff5fb660e01abc06c60541955
Henrique Vicente <henriquevicente@gmail.com>
2011-09-15 18:11:40 -0300

    re-structure Ml_Model_Time to follow the Zend coding standard
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- application/models/Ml/Time.php
+++ application/models/Ml/Time.php
@@ -6,19 +6,30 @@ class Ml_Model_Time
     {
         $zendDate = new Zend_Date();
         
-        if(!$format) $format = Zend_Date::ISO_8601;
+        if (! $format) {
+            $format = Zend_Date::ISO_8601;
+        }
+        
         $zendDate->set($date, $format);
         
         $ago = time()-$zendDate->getTimestamp();
         
-        if($ago < 120) return "less than $ago seconds ago";
+        if ($ago < 120) {
+            return "less than $ago seconds ago";
+        }
         
-        if($ago < 60*120) return ceil($ago/60)." minutes ago";
+        if ($ago < 60 * 120) {
+            return ceil($ago / 60) . " minutes ago";
+        }
         
-        if($ago < 60*60*48) return "about " . floor($ago/(60*60))." hours ago";
+        if ($ago < 60 * 60 * 48) {
+            return "about " . floor($ago / (60 * 60)) . " hours ago";
+        }
         
-        if($ago < 60*60*24*60) return floor($ago/(60*60*24))." days ago";
+        if ($ago < 60 * 60 * 24 * 60) {
+            return floor($ago / (60 * 60 * 24)) . " days ago";
+        }
         
-        return floor($ago/(60*60*24*7))." weeks ago";
+        return floor($ago / (60 * 60 * 24 * 7))." weeks ago";
     }
 }
