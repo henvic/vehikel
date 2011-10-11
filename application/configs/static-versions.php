55c15fe7e544cd87c053670d75e47d06659e8674
Henrique Vicente <henriquevicente@gmail.com>
2011-10-11 20:00:27 -0300

    simplify the versioning of the files at the static CDN
    Ignore the fact that the Amazon S3 doesn't provide auto-compression.
    
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- application/configs/static-versions.php
+++ application/configs/static-versions.php
@@ -3,31 +3,6 @@
  * This is required by the Ml_View_Helper_staticversion
  */
 
-$cacheFiles = array(
-//    "/javascript/password.js" => "/scripts/password-min.v3.js",
-//    "/javascript/progressbar.js" => "/scripts/progressbar-min.v4.js",
-//    "/javascript/username-avail.js" => "/scripts/username-avail-min.v4.js",
-    "/images/share-your-files.png" =>  "/images/share-your-files.v3.png",
-);
-
-/* sadly Amazon S3 doesn't use compression,
- * let's try this for the very basic files */
-$moreFiles = array(
-        "/javascript/plifk.js" => "/scripts/plifk-min.v4.js",
-        "/plifk.css" => "/style/plifk-min.v16.css",
-    );
-/*if(substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], "gzip"))
-{
-    $moreFiles = array(
-        "/javascript/plifk.js" => "/scripts/plifk-min.v1.gz.js",
-        "/plifk.css" => "/style/plifk-min.v14.gz.css",
-    );
-} else
-{
-    $moreFiles = array(
-        "/javascript/plifk.js" => "/scripts/plifk-min.v1.js",
-        "/plifk.css" => "/style/plifk-min.v14.css",
-    );
-}*/
-
-$cacheFiles = array_merge($cacheFiles, $moreFiles);
+$cacheFiles = array("/images/share-your-files.png" => "/images/share-your-files.v3.png",
+"/javascript/plifk.js" => "/scripts/plifk-min.v4.js",
+"/plifk.css" => "/style/plifk-min.v16.css");
