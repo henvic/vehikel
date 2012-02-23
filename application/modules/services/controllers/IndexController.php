f58e300f18dcd486a915e8e74d892efd47c4c87d
Henrique Vicente <henriquevicente@gmail.com>
2012-02-22 23:22:04 -0300

    change welcome message to a more useful one
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- application/modules/services/controllers/IndexController.php
+++ application/modules/services/controllers/IndexController.php
@@ -2,10 +2,6 @@
 class IndexController extends Zend_Controller_Action
 {
     public function indexAction() {
-        echo "There is not enough time.\n\nTry --help\n\n";
-        
-        if ($_SERVER['argc'] >= 2) {
-            echo "\nTip: have you forgot an --action|-a?\n\n";
-        }
+        echo "Welcome to the CLI SAPI.\nTry `--help` for more information.\n";
     }
 }
