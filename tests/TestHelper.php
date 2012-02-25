6c8439c3764ae6468501b4c5445d5fcab7cd37b9
Henrique Vicente <henriquevicente@gmail.com>
2012-02-25 05:16:32 -0300

    fix not working PHPUnit configuration
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- tests/TestHelper.php
+++ tests/TestHelper.php
@@ -1,10 +1,26 @@
 <?php
-define('BASE_PATH', realpath(dirname(__FILE__) . '/../'));
-define('APPLICATION_PATH', BASE_PATH . '/application');
-// Include path
-set_include_path('.' . PATH_SEPARATOR . BASE_PATH . '/library'
-. PATH_SEPARATOR .
- get_include_path());
+error_reporting(E_ALL | E_STRICT);
+
+define('HOST_MODULE', 'testing');
+
+defined('EXTERNAL_LIBRARY_PATH')
+    or define('EXTERNAL_LIBRARY_PATH',
+    realpath(getenv('EXTERNAL_LIBRARY_PATH')));
+
+defined('CACHE_PATH')
+    or define('CACHE_PATH', realpath(getenv('CACHE_PATH')));
+
+// Define path to application directory
+defined('APPLICATION_PATH')
+    || define('APPLICATION_PATH',
+              realpath(dirname(__FILE__) . '/../../application'));
+
+defined('LIBRARY_PATH')
+    or define('LIBRARY_PATH', realpath(APPLICATION_PATH . '/../library'));
+
 // Define application environment
-define('APPLICATION_ENV', 'testing');
-require 'ControllerTestCase.php';
+defined('APPLICATION_ENV')
+    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'testing'));
+
+set_include_path(implode(PATH_SEPARATOR,
+array(EXTERNAL_LIBRARY_PATH, LIBRARY_PATH)));
