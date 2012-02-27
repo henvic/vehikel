5d3cd32c9f18b8bbf7bd47f657c7b9886debdb43
Henrique Vicente <henriquevicente@gmail.com>
2012-02-27 06:26:06 -0300

    fix bootstrap for the testing environment
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- tests/TestHelper.php
+++ tests/TestHelper.php
@@ -1,6 +1,8 @@
 <?php
 error_reporting(E_ALL | E_STRICT);
 
+date_default_timezone_set(getEnv("DEFAULT_TIMEZONE"));
+
 define('HOST_MODULE', 'testing');
 
 defined('EXTERNAL_LIBRARY_PATH')
@@ -24,3 +26,15 @@ defined('APPLICATION_ENV')
 
 set_include_path(implode(PATH_SEPARATOR,
 array(EXTERNAL_LIBRARY_PATH, LIBRARY_PATH, get_include_path())));
+
+require EXTERNAL_LIBRARY_PATH . '/Zend/Loader/Autoloader.php';
+
+Zend_Loader_Autoloader::getInstance()->registerNamespace('Ml_');
+
+$resourceLoader =
+new Zend_Loader_Autoloader_Resource(array('basePath' => APPLICATION_PATH,
+'namespace' => 'Ml'));
+
+$resourceLoader->addResourceType('form', 'forms/', 'Form')
+->addResourceType('models', 'models/ML/', 'Model')
+->addResourceType('resources', 'resources/', 'Resource');
