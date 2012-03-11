8b0299440e944776b3370fe6efc574e90eb35f53
Henrique Vicente <henriquevicente@gmail.com>
2012-03-11 07:40:05 -0300

    add example of environment configuration file for development
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- application/configs/Environment.php
+++ application/configs/Environment.php
@@ -1,7 +1,13 @@
 <?php
-// Define application environment configuration
-//This configuration file might be created by bin/install
-define("APPLICATION_ENV", (defined("PHPUnit_MAIN_METHOD")) ? "testing" : "production");
+/**
+ * This is a example configuration file
+ * Make a copy of it to Environment.php.dist
+ * Or use the bin/install script to create and configure it
+ * 
+ * The values below are set with what you most likely want for development
+ */
+
+define("APPLICATION_ENV", (defined("PHPUnit_MAIN_METHOD")) ? "testing" : "development");
 define("EXTERNAL_LIBRARY_PATH", realpath(__DIR__ . "/../../vendor"));
 define("CACHE_PATH", realpath(__DIR__ . "/../../data/cache"));
 define("APPLICATION_CONF_FILE", realpath(__DIR__ . "/../../application/configs/application.ini.dist"));
