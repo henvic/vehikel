e54af4f621e79eee35fc4c6acf118b714d0f4354
Henrique Vicente <henriquevicente@gmail.com>
2012-03-17 16:56:18 -0300

    fix vendor file paths
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- application/configs/StaticVersions.php
+++ application/configs/StaticVersions.php
@@ -7,12 +7,12 @@ return array(
 "/images/share-your-files.png" => "/images/share-your-files.v3.png",
 //"/javascript/plifk.js" => "/scripts/plifk-min.v4.js",
 //"/plifk.css" => "/style/plifk-min.v16.css",
-"/external/jquery/jquery.min.js" => "/external/jquery-1.7.1/jquery.min.js",
-"/external/yui/yui-min.js" => "/external/yui-3.1.1/yui-min.js",
-"/external/jquery-fn-autoResize/jquery.autoresize.js" => "/external/jquery-fn-autoResize-1.14/jquery.autoresize.js",
-"/external/jquery-tablesorter/jquery.tablesorter.min.js" => "/external/jquery-tablesorter-2.0.5b/jquery.tablesorter.min.js",
-"/external/bootstrap/bootstrap-dropdown.js" => "/external/bootstrap-1.4.0/bootstrap-dropdown.js",
-"/external/bootstrap/bootstrap-twipsy.js" => "/external/bootstrap-1.4.0/bootstrap-twipsy.js",
-"/external/bootstrap/bootstrap-scrollspy.js" => "/external/bootstrap-1.4.0/bootstrap-scrollspy.js",
-"/external/bootstrap/bootstrap.min.css" => "/external/bootstrap-1.4.0/bootstrap.min.css"
+"/vendor/jquery/jquery.min.js" => "/vendor/jquery-1.7.1/jquery.min.js",
+"/vendor/yui/yui-min.js" => "/vendor/yui-3.1.1/yui-min.js",
+"/vendor/jquery-fn-autoResize/jquery.autoresize.js" => "/vendor/jquery-fn-autoResize-1.14/jquery.autoresize.js",
+"/vendor/jquery-tablesorter/jquery.tablesorter.min.js" => "/vendor/jquery-tablesorter-2.0.5b/jquery.tablesorter.min.js",
+"/vendor/bootstrap/js/bootstrap-dropdown.js" => "/vendor/bootstrap-1.4.0/js/bootstrap-dropdown.js",
+"/vendor/bootstrap/js/bootstrap-twipsy.js" => "/vendor/bootstrap-1.4.0/js/bootstrap-twipsy.js",
+"/vendor/bootstrap/js/bootstrap-scrollspy.js" => "/vendor/bootstrap-1.4.0/js/bootstrap-scrollspy.js",
+"/vendor/bootstrap/bootstrap.min.css" => "/vendor/bootstrap-1.4.0/bootstrap.min.css"
 );
