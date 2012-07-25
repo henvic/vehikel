b5030ecf9038534cb0326d9e70716f0a08c12556
Henrique Vicente <henriquevicente@gmail.com>
2012-07-24 23:54:40 -0300

    delete unrelated project-specific code
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- application/controllers/FaqController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class FaqController extends Zend_Controller_Action
-{
-    public function indexAction()
-    {
-        $this->_redirect($this->view->StaticUrl("/help"), array("exit"));
-    }
-}
--- application/views/helpers/Favorite.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Ml_View_Helper_Favorite extends Zend_View_Helper_Abstract
-{
-    public function favorite ()
-    {
-        return $this->view->render('shares/favorite.phtml');
-    }
-}
