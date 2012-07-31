78839d3a2d4057e5a1497bac8fc2b6b50734aea5
Henrique Vicente <henriquevicente@gmail.com>
2012-07-31 16:49:17 -0300

    update StaticVersion file versions
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- application/configs/StaticVersions.php
+++ application/configs/StaticVersions.php
@@ -4,15 +4,8 @@
  */
 
 return array(
-"/images/share-your-files.png" => "/images/share-your-files.v3.png",
-//"/javascript/plifk.js" => "/scripts/plifk-min.v4.js",
-//"/style/plifk.css" => "/style/plifk-min.v16.css",
-"/vendor/jquery/jquery.min.js" => "/vendor/jquery-1.7.1/jquery.min.js",
-"/vendor/yui/yui-min.js" => "/vendor/yui-3.1.1/yui-min.js",
+"/vendor/jquery/jquery.min.js" => "/vendor/jquery-1.7.2/jquery.min.js",
 "/vendor/jquery-fn-autoResize/jquery.autoresize.js" => "/vendor/jquery-fn-autoResize-1.14/jquery.autoresize.js",
 "/vendor/jquery-tablesorter/jquery.tablesorter.min.js" => "/vendor/jquery-tablesorter-2.0.5b/jquery.tablesorter.min.js",
-"/vendor/bootstrap/js/bootstrap-dropdown.js" => "/vendor/bootstrap-1.4.0/js/bootstrap-dropdown.js",
-"/vendor/bootstrap/js/bootstrap-twipsy.js" => "/vendor/bootstrap-1.4.0/js/bootstrap-twipsy.js",
-"/vendor/bootstrap/js/bootstrap-scrollspy.js" => "/vendor/bootstrap-1.4.0/js/bootstrap-scrollspy.js",
-"/vendor/bootstrap/bootstrap.min.css" => "/vendor/bootstrap-1.4.0/bootstrap.min.css"
+"/vendor/bootstrap/bootstrap.min.css" => "/vendor/bootstrap-2.0.4/bootstrap.min.css"
 );
