9555a0d0542fe717f88a4707a6f47005f0cf0cf8
Henrique Vicente <henriquevicente@gmail.com>
2012-08-02 09:56:27 -0300

    fix file paths
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- application/configs/StaticVersions.php
+++ application/configs/StaticVersions.php
@@ -4,8 +4,10 @@
  */
 
 return array(
-"/vendor/jquery/jquery.min.js" => "/vendor/jquery-1.7.2/jquery.min.js",
+"/vendor/jquery/jquery.js" => "/vendor/jquery-1.7.2/jquery.min.js",
 "/vendor/jquery-fn-autoResize/jquery.autoresize.js" => "/vendor/jquery-fn-autoResize-1.14/jquery.autoresize.js",
-"/vendor/jquery-tablesorter/jquery.tablesorter.min.js" => "/vendor/jquery-tablesorter-2.0.5b/jquery.tablesorter.min.js",
-"/vendor/bootstrap/bootstrap.min.css" => "/vendor/bootstrap-2.0.4/bootstrap.min.css"
+"/vendor/jquery-tablesorter/jquery.tablesorter.js" => "/vendor/jquery-tablesorter-2.0.5b/jquery.tablesorter.min.js",
+"/vendor/bootstrap/css/bootstrap.css" => "/vendor/bootstrap-2.0.4/css/bootstrap.min.css",
+"/vendor/bootstrap/css/bootstrap-responsive.css" => "/vendor/bootstrap-2.0.4/css/bootstrap-responsive.min.css",
+"/vendor/bootstrap/js/bootstrap.js" => "/vendor/bootstrap-2.0.4/js/bootstrap.min.js",
 );
