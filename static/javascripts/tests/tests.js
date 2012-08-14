7f1e129f826251c51bb661e2e9f4520f02c89208
Henrique Vicente <henriquevicente@gmail.com>
2012-08-13 21:59:45 -0300

    add qunit test suite
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

---
+++ static/javascripts/tests/tests.js
@@ -0,0 +1,9 @@
+/*globals asyncTest,deepEqual,equal,expect,module,notDeepEqual,notEqual, notStrictEqual,ok,QUnit,raises,start,stop,strictEqual,test */
+/*jshint indent:4 */
+var config = (function () {
+    "use strict";
+
+    test("hello test", function () {
+        ok("1" === "1", "Passed!");
+    });
+} ());
