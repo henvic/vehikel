c701c0acbdf92d9425a73c25ef99d81dfa86a87d
Henrique Vicente <henriquevicente@gmail.com>
2012-08-23 01:37:35 -0300

    remove not needed reference
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- application/configs/StaticVersions.php
+++ application/configs/StaticVersions.php
@@ -6,7 +6,6 @@
 return array(
 "/vendor/bootstrap/css/bootstrap.css" => "/vendor/bootstrap-2.0.4/css/bootstrap.min.css",
 "/vendor/bootstrap/css/bootstrap-responsive.css" => "/vendor/bootstrap-2.0.4/css/bootstrap-responsive.min.css",
-"/vendor/bootstrap/js/bootstrap.js" => "/vendor/bootstrap-2.0.4/js/bootstrap.min.js",
 "/vendor/require/require.js" => "/vendor/require-2.0.5/require.js",
 "/vendor/modernizr/modernizr.js" => "/vendor/modernizr-2.6.1/modernizr.js"
 );
