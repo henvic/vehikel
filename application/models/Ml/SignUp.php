cf0abd84d7678e808d1156626740c62613393618
Henrique Vicente <henriquevicente@gmail.com>
2012-08-24 00:04:19 -0300

    partial update of the join process (still to be completed)
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- application/models/Ml/SignUp.php
+++ application/models/Ml/SignUp.php
@@ -1,133 +1,53 @@
 <?php
-class Ml_Model_SignUp extends Ml_Model_AccessSingleton
+
+class Ml_Model_SignUp
 {
-    protected static $_dbTableName = "new_users";
-    
+    const REQUEST_EXPIRAL_TIME = 86400;
+
+    protected $_cache = null;
+    protected $_cachePrefix = "signup_req_";
+
+    public function __construct(Zend_Cache_Core $cache)
+    {
+        $this->_cache = $cache;
+    }
+
     /**
-     * Singleton instance
-     *
+     * Create a new sign up request and return data array with security code in success (else false)
+     * @param $name
+     * @param $email
+     * @return array|bool
      */
-    protected static $_instance = null;
-    
-    public static function signUpForm()
+    public function create($name, $email)
     {
-        static $form = '';
-        
-        if (! is_object($form)) {
-            $router = Zend_Controller_Front::getInstance()->getRouter();
-            
-            $form = new Ml_Form_SignUp(array(
-                'action' => $router->assemble(array(), "join"),
-                'method' => 'post',
-            ));
-        }
-        return $form;
-    }
-    
-    public static function newIdentityForm($securityCode)
-    {
-        static $form = '';
-        
-        if (! is_object($form)) {
-            $router = Zend_Controller_Front::getInstance()->getRouter();
-            
-            $form = new Ml_Form_NewIdentity(array(
-                'action' =>
-                 $router->assemble(array("security_code" => $securityCode),
-                 "join_emailconfirm"),
-                'method' => 'post',
-            ));
-        }
-        return $form;
-    }
-    
-    public function newUser($name, $email, $inviteCode = false)
-    {
-        //securitycode is just a random hexnumber
-        $securitycode = sha1($name . $email . mt_rand(- 54300, 105000) . microtime());
-        
-        $this->_dbAdapter->beginTransaction();
-        
-        $this->_dbAdapter->query('INSERT INTO ' . 
-        $this->_dbAdapter->quoteTableAs($this->_dbTable->getTableName()) .
-        ' (`email`, `name`, `timestamp`, `securitycode`) SELECT ?, ?, CURRENT_TIMESTAMP, ? FROM DUAL WHERE NOT EXISTS (select * from `people` where people.email = ?) ON DUPLICATE KEY UPDATE name=VALUES(name), timestamp=VALUES(timestamp), securitycode=VALUES(securitycode)',
-        array($email, $name, $securitycode, $email));
-        
-        if (! empty($inviteCode) &&
-         ! $this->_registry->isRegistered("inviteCompleteBefore") &&
-         ! $this->_registry->isRegistered("inviteMultiple")) {
-            $invites = Ml_Model_Invites::getInstance();
-            $invites->updateStatus($inviteCode, Ml_Model_Invites::USED);
-        }
-        
-        $this->_dbAdapter->commit();
-        
-        return array("name" => $name,
-        "email" => $email, 
-        "securitycode" => $securitycode);
-    }
-    
-    public function delete($id)
-    {
-        $delete = $this->_dbTable->delete($this->_dbAdapter->quoteInto('id = ?', $id));
-        
-        if ($delete) {
-            return true;
+        $securityCode = sha1($email . mt_rand() . microtime());
+
+        $data = array("name" => $name,
+            "email" => $email,
+            "securitycode" => $securityCode);
+
+        $saved = $this->_cache->save($data, $this->_cachePrefix . $securityCode, array(), self::REQUEST_EXPIRAL_TIME);
+
+        if ($saved) {
+            return $data;
         } else {
             return false;
         }
     }
-    
-    /**
-     * 
-     * Get sign up proccess by given email
-     * @param string $email
-     * @return array of sign up data
-     */
-    public function getByEmail($email)
+
+    public function read($securityCode)
     {
-        $select = $this->_dbTable->select()->where("binary email = ?", $email);
-        
-        $data = $this->_dbAdapter->fetchRow($select);
-        
-        if (is_object($data)) {
+        if (! ctype_xdigit($securityCode) || strlen($securityCode) != 40) {
             return false;
         }
-        
-        return $data;
+        return $this->_cache->load($this->_cachePrefix . $securityCode);
     }
-    
-    /**
-     * 
-     * Get sign up proccess by hash
-     * @param string $securityCode
-     * @return array of sign up data
-     */
-    public function getByHash($securityCode)
+
+    public function delete($securityCode)
     {
-        $select = $this->_dbTable->select();
-        
-        $select
-        ->where('securitycode = ?', $securityCode)
-        ->where('timestamp >= ?', date("Y-m-d H:i:s", time()-(48*60*60)));
-        
-        $confirmationInfo = $this->_dbAdapter->fetchRow($select);
-        
-        return $confirmationInfo;
-    }
-    
-	/**
-     * 
-     * Garbage collector
-     * @param int $maxAge in seconds
-     * @return number of deleted items
-     */
-    public function gc($maxAge)
-    {
-        $deleted = $this->_dbAdapter
-         ->delete($this->_dbTable->getTableName(), $this->_dbAdapter
-         ->quoteInto("timestamp < ?", date("Y-m-d H:i:s", time() - ($maxAge))));
-         
-         return $deleted;
+        if (! ctype_xdigit($securityCode) || strlen($securityCode) != 40) {
+            return false;
+        }
+        return $this->_cache->remove($this->_cachePrefix . $securityCode);
     }
 }
