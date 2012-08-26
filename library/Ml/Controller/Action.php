374a38043d5298d0010ac91a266dcdb07e6fb2ac
Henrique Vicente <henriquevicente@gmail.com>
2012-08-26 04:08:47 -0300

    add Ml_Controller_Action to help with simplifying the controllers
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

---
+++ library/Ml/Controller/Action.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Ml_Controller_Action helps in making the controllers simpler and more organized
+ */
+abstract class Ml_Controller_Action extends Zend_Controller_Action
+{
+    protected $_auth = null;
+    protected $_registry = null;
+    protected $_router = null;
+    protected $_sc = null;
+    protected $_config = null;
+
+    public function __construct(Zend_Controller_Request_Abstract $request, Zend_Controller_Response_Abstract $response, array $invokeArgs = array())
+    {
+        $this->_auth = Zend_Auth::getInstance();
+        $this->_registry = Zend_Registry::getInstance();
+        $this->_router = $this->getFrontController()->getRouter();
+
+        $sc = $this->_registry->get("sc");
+        /** @var $sc \Symfony\Component\DependencyInjection\ContainerBuilder() */
+        $this->_sc = $sc;
+
+        $this->_config = $this->_registry->get("config");
+
+        return parent::__construct($request, $response, $invokeArgs);
+    }
+}
