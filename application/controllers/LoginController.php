2a9f8ff04824ee426a85de74b47cceebdcd02d57
Henrique Vicente <henriquevicente@gmail.com>
2012-08-27 19:07:23 -0300

    refactor the login controller
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- application/controllers/LoginController.php
+++ application/controllers/LoginController.php
@@ -5,8 +5,8 @@
  *
  * @version    $Id:$
  * @since      File available since Release 0.1
-*/
-class LoginController extends Zend_Controller_Action
+ */
+class LoginController extends Ml_Controller_Action
 {
     /**
      * Redirects the user after sign in to
@@ -14,126 +14,112 @@ class LoginController extends Zend_Controller_Action
      */
     public function gobackAction()
     {
-        $registry = Zend_Registry::getInstance();
-        
-        $router = Zend_Controller_Front::getInstance()->getRouter();
-        
-        $config = $registry->get("config");
-        
-        $credential = Ml_Model_Credential::getInstance();
-        
+        $credential =  $this->_sc->get("credential");
+        /** @var $credential \Ml_Model_Credential() */
+
         $redirLink = $credential->checkLinkToRedirect();
-        
+
         if (! $redirLink) {
-            $redirLink = $router->assemble(array(), "index");
+            $redirLink = $this->_router->assemble(array(), "index");
         }
-        
-        //never to use $config['webroot'] . here because $redirLink already contains it
-        $this->_redirect("http://" . $config['webhost'] . $redirLink, array("exit"));
+
+        //never to use $this->_config['webroot'] . here because $redirLink already contains it
+        $this->_redirect("http://" . $this->_config['webhost'] . $redirLink, array("exit"));
     }
-    
+
     /**
      * Redirects the user to the login page
      */
     public function redirectAction()
     {
-        $registry = Zend_Registry::getInstance();
-        
-        $router = Zend_Controller_Front::getInstance()->getRouter();
-        
-        $config = $registry->get("config");
-        
-        $request = $this->getRequest();
-        
-        $params = $request->getParams();
-        
-        if ($config['ssl']) {
-            $loginFallback = "https://" . $config['webhostssl'];// no place for $config['webroot'] here
+        $params = $this->_request->getParams();
+
+        if ($this->_config['ssl']) {
+            $loginFallback = "https://" . $this->_config['webhostssl'];// no place for $this->_config['webroot'] here
         } else {
-            $loginFallback = "http://" . $config['webhost'];
+            $loginFallback = "http://" . $this->_config['webhost'];
         }
-        
+
         $loginFallback  .=
-        $router->assemble(array(), "login") . "?redirect_after_login=" .
-        $router->assemble($params, $router->getCurrentRouteName());
-        
+            $this->_router->assemble(array(), "login") . "?redirect_after_login=" .
+                $this->_router->assemble($params, $this->_router->getCurrentRouteName());
+
         $this->_redirect($loginFallback, array("exit"));
     }
-    
+
     public function indexAction()
     {
-        $registry = Zend_Registry::getInstance();
-        $auth = Zend_Auth::getInstance();
-        
-        $config = $registry->get("config");
-        $sessionConfig = $config['resources']['session'];
-        
+        $sessionConfig = $this->_config['resources']['session'];
+
         Ml_Model_AntiAttack::loadRules();
-        $credential = Ml_Model_Credential::getInstance();
-        $logger = Ml_Model_Logger::getInstance();
-        
-        if ($auth->hasIdentity()) {
+        $credential =  $this->_sc->get("credential");
+        /** @var $credential \Ml_Model_Credential() */
+
+        $logger =  $this->_sc->get("logger");
+        /** @var $logger \Ml_Logger() */
+
+        if ($this->_auth->hasIdentity()) {
             return $this->_forward("goback");
         }
-        
-        $request = $this->getRequest();
-        $form = $credential->loginForm();
-        
+
+        $form =  $this->_sc->get("login_form");
+        /** @var $form \Ml_Form_Login() */
+
         if (Ml_Model_AntiAttack::ensureHuman()) {
             $ensureHuman = true;
         } else {
             $ensureHuman = false;
         }
-        
-        
-        if ($request->isPost()) {
-        
-        ignore_user_abort(true);
-        
-        //A way to sign in only if captcha is right. This is a workaround to
-        //signout if the captcha is wrong.
-        //
-        //I've decided to put the sign in code in the validator itself,
-        //but couldn't find a way to make the password validator
-        //load after the captcha one (but to let it come first in code,
-        //and that's ugly on the screen) and get a result if the
-        //validation worked. Notice that it is only useful when
-        //the captcha is required.
-        if ($form->isValid($request->getPost())) {//@see below
-            $session = Ml_Model_Session::getInstance();
-            
-            //rememberMe and ForgetMe already regenerates the ID
-            if ($form->getElement("remember_me")->isChecked()) {
-                Zend_Session::rememberMe($sessionConfig['cookie_lifetime']);
+
+        if ($this->_request->isPost()) {
+
+            ignore_user_abort(true);
+
+            //A way to sign in only if captcha is right. This is a workaround to
+            //signout if the captcha is wrong.
+            //
+            //I've decided to put the sign in code in the validator itself,
+            //but couldn't find a way to make the password validator
+            //load after the captcha one (but to let it come first in code,
+            //and that's ugly on the screen) and get a result if the
+            //validation worked. Notice that it is only useful when
+            //the captcha is required.
+            if ($form->isValid($this->_request->getPost())) {//@see below
+                $session =  $this->_sc->get("session");
+                /** @var $session \Ml_Session() */
+
+                //rememberMe and ForgetMe already regenerates the ID
+                if ($form->getElement("remember_me")->isChecked()) {
+                    Zend_Session::rememberMe($sessionConfig['cookie_lifetime']);
+                } else {
+                    Zend_Session::ForgetMe();
+                }
+
+                $session->associate($this->_auth->getIdentity(), Zend_Session::getId());
+
+                $logger->log(array("action" => "login",
+                    "username" => $form->getValue("username")));
+
+                $this->_forward("goback");
             } else {
-                Zend_Session::ForgetMe();
-            }
-            
-            $session->associate($auth->getIdentity(), Zend_Session::getId());
-            
-            $logger->log(array("action" => "login",
-            "username" => $form->getValue("username")));
-            
-            $this->_forward("goback");
-        } else {
-            //@see above
-            if ($auth->hasIdentity()) {
-                $auth->clearIdentity();
-            }
-            
-            $logger->log(array("action" => "login_denied",
-            "username" => $form->getValue("username")));
-            
-            $this->view->errorlogin = true;
-        }//@end of workaround
+                //@see above
+                if ($this->_auth->hasIdentity()) {
+                    $this->_auth->clearIdentity();
+                }
+
+                $logger->log(array("action" => "login_denied",
+                    "username" => $form->getValue("username")));
+
+                $this->view->errorlogin = true;
+            }//@end of workaround
         }
         $challenge = $form->getElement("challenge");
-        
+
         //don't show missing value in the first time that asks for the captcha
         if (! $ensureHuman && is_object($challenge)) {
             $challenge->setErrorMessages(array("missingValue" => ''));
         }
-        
+
         $this->view->loginform = $form;
     }
 }
