2a450b611694f49d52faf357249fd9db559223a7
Henrique Vicente <henriquevicente@gmail.com>
2012-08-27 06:03:18 -0300

    refactor the login form
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- application/forms/Login.php
+++ application/forms/Login.php
@@ -1,67 +1,84 @@
 <?php
 
-class Ml_Form_Login extends Zend_Form
+class Ml_Form_Login extends Twitter_Bootstrap_Form_Horizontal
 {
+    protected $_config = null;
+
+    /**
+     * @param null $options
+     * @param Zend_Config array $config
+     */
+    public function __construct($options = null, array $config)
+    {
+        $this->_config = $config;
+
+        return parent::__construct($options);
+    }
+
     public function init()
     {
-        $registry = Zend_Registry::getInstance();
-        $config = $registry->get("config");
-        
-        $this->addElementPrefixPath('Ml_Validate', 'Ml/Validate/', 
-        Zend_Form_Element::VALIDATE);
-        $this->addElementPrefixPath('Ml_Filter', 'Ml/Filter/', 
-        Zend_Form_Element::FILTER);
-        
+        $this->setMethod('post');
+
+        if ($this->_config['ssl']) {
+            $url = 'https://' . $this->_config['webhostssl'];
+        } else {
+            $url = '';
+        }
+
+        $url .= $this->getView()->url(array(), "login");
+        $this->setAction($url);
+
+        $this->addElementPrefixPath('Ml_Validate', 'Ml/Validate/',
+            Zend_Form_Element::VALIDATE);
+        $this->addElementPrefixPath('Ml_Filter', 'Ml/Filter/',
+            Zend_Form_Element::FILTER);
+
         $this->addElement('text', 'username', array(
-            'label'      => 'Username or e-mail:',
+            'label'      => 'Usuário ou email',
             'required'   => true,
             'autofocus' => 'autofocus',
             'filters'    => array('StringTrim', 'StringToLower'),
             'validators' => array(
                 array('validator' => 'username'), //stringlenght there
-                )
+            )
         ));
-        
+
         $this->getElement("username")->setAttrib('required', 'required');
-        
+
         $this->addElement('password', 'password', array(
-            'label'      => 'Password:',
+            'label'      => 'Senha',
             'required'   => true,
-            'filters'    => array('StringTrim'),
             'validators' => array(
-        array('validator' => 'StringLength', 'options' => array(5, 20)),
+                array('validator' => 'StringLength', 'options' => array(5, 20)),
                 array('validator' => 'password'),
             ),
         ));
-        
+
         $this->getElement("password")->setAttrib('required', 'required');
-        
+
         $this->addElement('checkbox', 'remember_me', array(
-            'label'    => 'Remember me'));
-        
+            'label'    => 'Relembrar-me'));
+
         if (Ml_Model_AntiAttack::ensureHuman()) {
             $this->addElement(Ml_Model_AntiAttack::captchaElement());
         }
-        
+
         $login = $this->addElement('submit', 'login', array(
             'required' => false,
             'ignore'   => true,
-            'label'    => 'Sign in',
-            'class'    => 'btn primary',
+            'label'    => 'Entrar',
+            'class'    => 'btn',
         ));
-        
-        
-        if ($config['ssl']) {
+
+
+        if ($this->_config['ssl']) {
             $this->getElement("login")->addValidator("Https");
-            
+
             //By default the submit element doesn't display a error decorator
             $this->getElement("login")->addDecorator("Errors");
         }
-        
-        $this->getElement("username")->setAttrib('class', 'span3');
-        $this->getElement("password")->setAttrib('class', 'span3');
-        
+
         $this->setAttrib('class', 'form-stacked');
     }
 }
- 
+
