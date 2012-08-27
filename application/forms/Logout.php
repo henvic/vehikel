10b1d1fc26f5c2b871931d769e52c729193d0349
Henrique Vicente <henriquevicente@gmail.com>
2012-08-27 18:53:54 -0300

    improve logout form
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- application/forms/Logout.php
+++ application/forms/Logout.php
@@ -3,6 +3,10 @@ class Ml_Form_Logout extends Zend_Form
 {
     public function init()
     {
+        $url = $this->getView()->url(array(), "logout");
+        $this->setAction($url);
+        $this->setMethod('post');
+
         $this->addElementPrefixPath('Ml_Validate', 'Ml/Validate/', 
         Zend_Form_Element::VALIDATE);
         $this->addElementPrefixPath('Ml_Filter', 'Ml/Filter/', 
