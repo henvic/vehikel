a19b4be2fcc9ded22d5a9441f87e2813ec85b224
Henrique Vicente <henriquevicente@gmail.com>
2012-08-27 23:40:42 -0300

    reorganize code and remove old confusing @todo message
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- application/resources/Mysession.php
+++ application/resources/Mysession.php
@@ -12,14 +12,13 @@ class Ml_Resource_Mysession extends Zend_Application_Resource_ResourceAbstract
         $sessionConfig = $config['resources']['session'];
         
         $cookieLifetime = $sessionConfig['cookie_lifetime'];
-        
-        /* @todo fix issue of system with incoherent behavior when the session
-        system has a issue, such as when the savehandler doesn't work as
-        expected when it's off-line which results in differents
-        catched / uncatched exception when the resource (page) loads
-        */
-        $saveHandler = new Ml_Session_SaveHandler_PlusCache($registry->get("memCache"),
-        $config['session']['prefix'], $config['lastActivity']['prefix']);
+
+        $saveHandler = new Ml_Session_SaveHandler_PlusCache(
+            $registry->get("memCache"),
+            $config['session']['prefix'],
+            $config['lastActivity']['prefix'],
+            $auth
+        );
         
         Zend_Session::setSaveHandler($saveHandler);
         
