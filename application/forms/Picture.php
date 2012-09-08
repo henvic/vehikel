c61d824ce0547df193c75e004369c404fa79def5
Henrique Vicente <henriquevicente@gmail.com>
2012-09-08 02:40:27 -0300

    refactor the Picture form
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- application/forms/Picture.php
+++ application/forms/Picture.php
@@ -1,39 +1,52 @@
 <?php
 
-class Ml_Form_Picture extends Zend_Form
+class Ml_Form_Picture extends Twitter_Bootstrap_Form_Horizontal
 {
+    public $_max_picture_size = 2097152; //2048 * 1024
+
     public function init()
     {
         $this->setAttrib('enctype', 'multipart/form-data');
-        
+
         $this->setMethod('post');
+        $this->setAction($this->getView()->url(array(), 'accountpicture'));
+
         $this->addElementPrefixPath('Ml_Validate', 'Ml/Validate/', 
         Zend_Form_Element::VALIDATE);
         $this->addElementPrefixPath('Ml_Filter', 'Ml/Filter/', 
         Zend_Form_Element::FILTER);
         
         $file = new Zend_Form_Element_File('Image');
-        $file->setLabel('Choose a picture:');
+        $file->setLabel('Escolha uma imagem:');
         $file->addValidator('Count', false, 1);
         $file->addValidator('Size', false, array('max' => '1MB'));
-        $file->setMaxFileSize(2048*1024);
+        $file->setMaxFileSize($this->_max_picture_size);
         $file->addValidator('Extension', false, 'jpg,png,gif');
         $file->setRequired(false);
         $file->setOptions(Array('ignoreNoFile' => true));
         $this->addElement($file, 'Image');
         
         $this->addElement('submit', 'submit', array(
-            'label'    => 'Submit!',
-            'class'    => 'btn primary',
+            'label'    => 'Enviar',
+            'class'    => 'btn btn-primary',
         ));
         
         $this->addElement('submit', 'delete', array(
-            'label'    => 'Delete current',
-            'class'    => 'btn danger',
+            'label'    => 'Remover atual',
+            'class'    => 'btn btn-danger',
         ));
-        
-        $this->setAttrib('class', 'form-stacked');
-        
+
         $this->addElement(Ml_Model_MagicCookies::formElement());
+
+        $this->addDisplayGroup(
+            array('submit', 'reset'),
+            'actions',
+            array(
+                'disableLoadDefaultDecorators' => true,
+                'decorators' => array('Actions')
+            )
+        );
+
+        $this->setAttrib('class', 'form-stacked');
     }
 }
