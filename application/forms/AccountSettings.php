bd2ab2a8178171dc5a9374dd97bb6211e6738f02
Henrique Vicente <henriquevicente@gmail.com>
2012-09-08 02:40:42 -0300

    refactor the Account Settings form
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- application/forms/AccountSettings.php
+++ application/forms/AccountSettings.php
@@ -1,26 +1,36 @@
 <?php
-class Ml_Form_AccountSettings extends Zend_Form
+class Ml_Form_AccountSettings extends Twitter_Bootstrap_Form_Horizontal
 {
     public function init()
     {
         $this->setMethod('post');
+        $this->setAction($this->getView()->url(array(), "account"));
+
         $this->addElementPrefixPath('Ml_Validate', 'Ml/Validate/', 
         Zend_Form_Element::VALIDATE);
         $this->addElementPrefixPath('Ml_Filter', 'Ml/Filter/', 
         Zend_Form_Element::FILTER);
-        
+
         $this->addElement('text', 'name', array(
-            'label'      => 'Name:',
+            'label'      => 'Nome',
             'required'   => true,
             'filters'    => array('StringTrim'),
             'validators' => array(
                 array('validator' =>
-                    'StringLength', 'options' => array(1, 50))
-                )
+                'StringLength', 'options' => array(1, 50))
+            ),
         ));
-        
+
+        $this->addElement('text', 'username', array(
+            'label'      => 'Seu nome de usuário',
+            'required'   => true,
+            'class'      => 'input-small',
+            'prepend' => '<i class="icon-user"></i>',
+            'readonly' => true
+        ));
+
         $email = $this->addElement('text', 'email', array(
-            'label'      => 'E-mail address:',
+            'label'      => 'Endereço de email',
             'required'   => true,
             'filters'    => array('StringTrim', 'StringToLower'),
             'validators' => array(
@@ -29,14 +39,20 @@ class Ml_Form_AccountSettings extends Zend_Form
                 array('validator' =>
                     'emailCheckUser'), //stringlenght there also
                 array('validator' => 'EmailAddress')
-                )
+                ),
+            'prepend' => '<i class="icon-envelope"></i>',
         ));
-        
-        $emailprivacy = $this->addElement('checkbox', 'private_email', array(
-            'label'    => 'Hide e-mail from public search'));
-        
+
+        $emailprivacy = $this->addElement(
+            'checkbox',
+            'private_email',
+            array(
+                'label'    => 'Não mostrar email publicamente',
+            )
+        );
+
         $website = $this->addElement('text', 'website', array(
-            'label'      => 'Your homepage:',
+            'label'      => 'Seu site',
             'required'   => false,
             'filters'    => array('StringTrim', 'UrlFilter'),
             'validators' => array(
@@ -45,9 +61,9 @@ class Ml_Form_AccountSettings extends Zend_Form
                 array('validator' => 'Url')
                 )
         ));
-        
+
         $location = $this->addElement('text', 'location', array(
-            'label'      => 'Location:',
+            'label'      => 'Localidade',
             'required'   => false,
             'filters'    => array('StringTrim'),
             'validators' => array(
@@ -55,28 +71,40 @@ class Ml_Form_AccountSettings extends Zend_Form
                     'StringLength', 'options' => array(1, 40)),
                 )
         ));
-        
+
         $about = $this->addElement('textarea', 'about', array(
-            'label'      => 'About:',
+            'label'      => 'Sobre',
             'description' =>
                 '<small><a href="' .
                 $this->getView()->staticUrl("/help/html") .
-                '" class="new-window">HTML formatting</a></small>',
+                '" rel="external">HTML formatting</a></small>',
             'required'   => false,
             'filters'    => array('StringTrim'),
             'validators' => array(
                 array('validator' =>
                     'StringLength', 'options' => array(0, 4096)),
-                )
+                ),
+            'rows' => 5
         ));
-        
+
+        $this->getElement('about')->getDecorator('description')->setEscape(false);
+
         $this->addElement('submit', 'submit', array(
-            'label'    => 'Save!',
-            'class' => 'btn primary'
+            'label'    => 'Salvar',
+            'class' => 'btn btn-primary'
         ));
-        
+
         $this->addElement(Ml_Model_MagicCookies::formElement());
-        
+
+        $this->addDisplayGroup(
+            array('submit', 'reset'),
+            'actions',
+            array(
+                'disableLoadDefaultDecorators' => true,
+                'decorators' => array('Actions')
+            )
+        );
+
         $this->setAttrib('class', 'form-stacked');
     }
 }
