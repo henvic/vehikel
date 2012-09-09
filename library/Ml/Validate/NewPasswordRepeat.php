c266e8d73fa03d0b9bf9aa27b3efdef58af94b59
Henrique Vicente <henriquevicente@gmail.com>
2012-09-08 23:55:00 -0300

    refactor NewPasswordRepeat
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- library/Ml/Validate/NewPasswordRepeat.php
+++ library/Ml/Validate/NewPasswordRepeat.php
@@ -1,34 +1,27 @@
 <?php
-//require_once 'Zend/Validate/Abstract.php';
-
 
 class Ml_Validate_NewPasswordRepeat extends Zend_Validate_Abstract
 {
     const NOT_MATCH = 'notMatch';
 
     protected $_messageTemplates = array(
-        self::NOT_MATCH => 'Password confirmation does not match'
+        self::NOT_MATCH => 'A confirmação de senha não corresponde à senha'
     );
 
     public function isValid($value, $context = null)
     {
         $value = (string) $value;
         $this->_setValue($value);
-        
-        if (mb_strlen($value) < 6 || mb_strlen($value) > 20) {
+
+        if (! isset($context["password_confirm"])) {
+            throw new Exception("password_confirm shall be set for using the NewPasswordRepeat validate");
+        }
+
+        if ($value != $context["password_confirm"]) {
+            $this->_error(self::NOT_MATCH);
             return false;
         }
 
-        if (is_array($context)) {
-            if (isset($context['password_confirm'])
-                && ($value == $context['password_confirm'])) {
-                return true;
-            }
-        } else if (is_string($context) && ($value == $context)) {
-            return true;
-        }
-
-        $this->_error(self::NOT_MATCH);
-        return false;
+        return true;
     }
 }
