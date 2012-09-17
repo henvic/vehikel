4b37349d8c0fcca60db57564f74cd461e83efee6
Henrique Vicente <henriquevicente@gmail.com>
2012-09-17 04:01:49 -0300

    refactor the People model and add a cache layer to it
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- application/models/Ml/People.php
+++ application/models/Ml/People.php
@@ -1,96 +1,165 @@
 <?php
-class Ml_Model_People extends Ml_Model_AccessSingleton
+class Ml_Model_People
 {
-    protected static $_dbTableName = "people";
-    
-    /**
-     * Singleton instance
-     *
-     */
-    protected static $_instance = null;
-    
-    public function getByUsername($username)
+    const CACHE_LIFETIME = 60;
+
+    // this number should increase after relevant changes
+    const CACHE_OBJECT_VERSION = 0;
+
+    protected $_dbTableName = "people";
+    protected $_dbHistoryTableName = "people_history";
+    protected $_dbAdapter;
+    protected $_dbTable;
+
+    protected $_cache;
+    protected $_cachePrefix = "user_";
+    protected $_cacheUsernamePrefix = "user_name_";
+
+    public function __construct($config, Zend_Cache_Core $cache)
     {
-        $select = $this->_dbTable->select()
-        ->where("binary `alias` = ?", $username);
-        
-        return $this->_dbAdapter->fetchRow($select);
+        $this->_cache = $cache;
+        $this->_dbTable = new Zend_Db_Table($this->_dbTableName, $config);
+        $this->_dbAdapter = $this->_dbTable->getAdapter();
     }
-    
+
+    public function getByUsername($username, $useCache = true)
+    {
+        if ($useCache) {
+            $cached = $this->_cache->load($this->_cacheUsernamePrefix . $username);
+            if ($cached && $cached["cache"] == self::CACHE_OBJECT_VERSION) {
+                return $cached;
+            }
+        }
+
+        $select = $this->_dbTable->select()
+            ->where("binary `username` = ?", $username);
+
+        return $this->getDbResult($select);
+    }
+
     public function getByEmail($email)
     {
         $select = $this->_dbTable->select()
-        ->where("binary `email` = ?", $email);
-        
-        return $this->_dbAdapter->fetchRow($select);
+            ->where("binary `email` = ?", $email);
+
+        return $this->getDbResult($select);
     }
-    
-    public function getById($uid)
+
+    /**
+     * @param $value value is username or email
+     * @param bool $useCache if cache might be used or not, it is ignored if email
+     */
+    public function get($value, $useCache = true)
     {
-        return $this->_dbTable->getById($uid);
+        if (strpos($value, '@') === false) {
+            return $this->getByUsername($value, $useCache);
+        }
+        return $this->getByEmail($value);
     }
-    
+
+    public function getById($uid, $useCache = true)
+    {
+        if ($useCache) {
+            $cached = $this->_cache->load($this->_cachePrefix . $uid);
+            if ($cached && $cached["cache"] == self::CACHE_OBJECT_VERSION) {
+                return $cached;
+            }
+        }
+
+        $select = $this->_dbTable->select()->where("binary `id` = ?", $uid);
+
+        return $this->getDbResult($select);
+    }
+
     public function update($uid, $data)
     {
         $update = $this->_dbTable->update($data, $this->_dbAdapter->quoteInto("id = ?", $uid));
+
         if ($update) {
+            $this->saveHistorySnapshot($uid);
+            //retrieves fresh data renewing the cached values in the process
+            $this->getById($uid, false);
             return true;
         }
-        
+
         return false;
     }
-    
-    public function create($username, $password, $data, $confirmationInfo)
+
+    public function create($username, $name, $email)
     {
-        $signUp = Ml_Model_SignUp::getInstance();
-        $credential = Ml_Model_Credential::getInstance();
-        $profile = Ml_Model_Profile::getInstance();
-        
         $this->_dbAdapter->beginTransaction();
         try {
-            $signUp->delete($confirmationInfo['id']);
-            
-            $this->_dbTable->insert($data);
-            
+            $this->_dbTable->insert(array("alias" => $username, "username" => $username, "name" => $name, "email" => $email));
+
             $uid = $this->_dbAdapter->lastInsertId();
-            
+
             if (! $uid) {
-                throw new Exception("Failed to create user account");
+                return false;
             }
-            
-            $credential->setCredential($uid, $password);
-            $profile->create($uid);
+
+            $this->saveHistorySnapshot($uid);
+
             $this->_dbAdapter->commit();
         } catch (Exception $e) {
             $this->_dbAdapter->rollBack();
             throw $e;
         }
-        
+
         return $uid;
     }
-    
-    public function delete($uid)
+
+    protected function getDbResult(Zend_Db_Select $sql)
     {
-        return $this->_dbTable->delete($this->_dbAdapter->quoteInto("id = ?", $uid));
+        $userInfo = $this->_dbAdapter->fetchRow($sql);
+
+        if (is_array($userInfo)) {
+            $userInfo["cache"] = false;
+            $userInfo["avatarInfo"] = json_decode($userInfo["avatarInfo"], true);
+
+            $this->setCache($userInfo);
+
+            return $userInfo;
+        }
+
+        return false;
     }
-    
-    public function joinDbTableInfo(&$select, $parentTable, $uidField)
-    {    
-        $select->from($parentTable);
-        $select->setIntegrityCheck(false);
-        
-        $select->joinLeft("people",
-         "people.id = " . $parentTable . "." . $uidField,
-         array("people.id as people.id",
-          "people.alias as people.alias",
-          "people.name as people.name",
-          "people.avatarInfo as people.avatarInfo"));
-         
-        $select->joinLeft("people_deleted",
-         "people.id IS NULL AND people_deleted.id = " .
-         $parentTable . "." . $uidField,
-          array("people_deleted.id as people_deleted.id",
-           "people_deleted.alias as people_deleted.alias", 
-           "people_deleted.name as people_deleted.name"));
+
+    /**
+     * Sets two caches: one with ID and another with username as keys
+     *
+     * @param $userInfo
+     */
+    protected function setCache($userInfo)
+    {
+        $userInfo["cache"] = self::CACHE_OBJECT_VERSION;
+
+        $this->_cache->save($userInfo, $this->_cachePrefix . $userInfo["id"], array(), self::CACHE_LIFETIME);
+
+        $this->_cache->save(
+            $userInfo,
+            $this->_cacheUsernamePrefix . $userInfo["username"],
+            array(),
+            self::CACHE_LIFETIME
+        );
+    }
+
+    protected function deleteCache($id, $username)
+    {
+        $this->_cache->remove($this->_cachePrefix . $id);
+        $this->_cache->remove($this->_cacheUsernamePrefix . $username);
+    }
+
+    protected function saveHistorySnapshot($id)
+    {
+        //the history is built using a best effort, non-transational way
+        $historySql = "INSERT INTO "
+            . $this->_dbAdapter->quoteTableAs($this->_dbHistoryTableName)
+            . "SELECT UUID() as history_id, "
+            . $this->_dbAdapter->quoteTableAs($this->_dbTableName)
+            . ".*, CURRENT_TIMESTAMP as change_time FROM "
+            . $this->_dbAdapter->quoteTableAs($this->_dbTableName)
+            . "WHERE id = :id";
+
+        $this->_dbAdapter->query($historySql, array("id" => $id));
     }
 }
