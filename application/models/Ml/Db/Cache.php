25490aadab7f8e8a40bfde620f02efa68ad6a552
Henrique Vicente <henriquevicente@gmail.com>
2012-09-18 03:05:01 -0300

    add caching trait
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

---
+++ application/models/Ml/Db/Cache.php
@@ -0,0 +1,29 @@
+<?php
+
+trait Ml_Model_Db_Cache
+{
+    /** @var $_cache \Zend_Cache_Core() */
+    protected $_cache;
+
+    protected function getCache($id)
+    {
+        return $this->_cache->load($this->getCachePath($id));
+    }
+
+    protected function setCache($id, array $data)
+    {
+        $data["cache"] = $this->_cacheObjectVersion;
+
+        $this->_cache->save($data, $this->getCachePath($id), array(), $this->_cacheLifetime);
+    }
+
+    protected function deleteCache($id)
+    {
+        $this->_cache->remove($this->getCachePath($id));
+    }
+
+    protected function getCachePath($id)
+    {
+        return $this->_cachePrefix . $this->_cacheObjectVersion . "_" . $id;
+    }
+}
