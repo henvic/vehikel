5e1c1c6af6b846ed43818c6aba5d792a698ef4a6
Henrique Vicente <henriquevicente@gmail.com>
2012-10-05 00:31:52 -0300

    add the AddressPhoneList view helper
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

---
+++ application/views/helpers/AddressPhoneList.php
@@ -0,0 +1,33 @@
+<?php
+
+class Ml_View_Helper_AddressPhoneList extends Zend_View_Helper_Abstract
+{
+    public function addressPhoneList($phoneList)
+    {
+        $content = '<ul>';
+
+        foreach($phoneList as $phone) {
+            $formattedBrPhone = sprintf("%s %s-%s",
+                mb_substr($phone["tel"], 3, 2),
+                mb_substr($phone["tel"], 5, 4),
+                mb_substr($phone["tel"], 9)
+            );
+
+            $content .= '<li>'
+                . '<a class="tel" href="tel:'
+                . $this->view->escape($phone["tel"])
+                . '">☎ '
+                . $this->view->escape($formattedBrPhone);
+
+            if ($phone["name"]) {
+                $content .= ' - ' . $this->view->escape($phone["name"]);
+            }
+
+            $content .= '</a></li>';
+        }
+
+        $content .= '</ul>';
+
+        return $content;
+    }
+}
