c218749b9a0b0d9c5a925f8119274b30df92f075
Henrique Vicente <henriquevicente@gmail.com>
2012-10-08 19:53:47 -0300

    add business rules for contacting seller
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- application/controllers/UserPostController.php
+++ application/controllers/UserPostController.php
@@ -6,11 +6,50 @@ class UserPostController extends Ml_Controller_Action
 
     public function indexAction()
     {
+        $userInfo = $this->_userInfo;
+
+        $this->view->addJsParam("route", "user/post");
+
         $people =  $this->_registry->get("sc")->get("people");
         /** @var $people \Ml_Model_People() */
 
-        $form = new Ml_Form_ContactCarDealer();
+        $form = new Ml_Form_ContactSeller(null, $this->_userInfo["username"], $this->_post["id"]);
 
-        $this->view->contactCarDealerForm = $form;
+        $this->view->contactSellerForm = $form;
+
+        if ($this->getRequest()->isPost()) {
+            if ($form->isValid($this->getRequest()->getPost())) {
+                $contactSeller =  $this->_registry->get("sc")->get("contactSeller");
+                /** @var $contactSeller \Ml_Model_ContactSeller() */
+
+                $values = $form->getValues();
+
+                $contactSeller->saveMessage(
+                    $userInfo,
+                    $values["name"],
+                    $values["email"],
+                    $values["phone"],
+                    $values["message"]
+                );
+
+                $this->view->contact = $values;
+
+                $mail = new Zend_Mail('UTF-8');
+
+                $mail->setBodyText($this->view->render("user-post/contact-message.phtml"))
+                    ->addTo($userInfo['email'], $userInfo['name'])
+                    ->setSubject('Proposta')
+                    ->setReplyTo($values["email"])
+                    ->send();
+
+                $this->view->proposalSent = true;
+            }
+        }
+
+        // out of the isPost because we might want to use GET sometimes
+        if ($this->getRequest()->isXmlHttpRequest()) {
+            $this->_helper->layout->disableLayout();
+            $this->render("partial-contact");
+        }
     }
 }
