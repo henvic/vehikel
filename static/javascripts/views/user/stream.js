dead5a78262d248fdff0e33001e158f3b4ae6727
Henrique Vicente <henriquevicente@gmail.com>
2012-10-08 19:56:38 -0300

    add user/stream.js to toggle the view style with ajax
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

---
+++ static/javascripts/views/user/stream.js
@@ -0,0 +1,35 @@
+/*global define */
+define(['jquery'], function($) {
+    "use strict";
+
+    var userStreamPostsElement = $('#user-stream-posts');
+
+    var postsViewStyleThumbnailRadio = $('#posts_view_style_thumbnail');
+    var postsViewStyleTableRadio = $('#posts_view_style_table');
+
+    var streamCache = {};
+
+    var changeViewStyle = function(style) {
+        // at first tries to retrieve from the cache
+        if (streamCache[style]) {
+            userStreamPostsElement.html(streamCache[style]);
+            return;
+        }
+
+        $.ajax({
+            url: '?posts_view_style=' + style,
+            success: function(data) {
+                streamCache[style] = data;
+                userStreamPostsElement.html(data);
+            }
+        });
+    }
+
+    postsViewStyleTableRadio.on("click", function() {
+        changeViewStyle("table");
+    });
+
+    postsViewStyleThumbnailRadio.on("click", function() {
+        changeViewStyle("thumbnail");
+    });
+});
