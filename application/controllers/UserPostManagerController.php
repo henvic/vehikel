d97926258047f2ef1acf77a3d46e119cd66bd994
Henrique Vicente <henriquevicente@gmail.com>
2012-11-03 18:18:11 -0300

    add the pictureAdd action for the UserPostManager controller
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- application/controllers/UserPostManagerController.php
+++ application/controllers/UserPostManagerController.php
@@ -4,4 +4,53 @@ class UserPostManagerController extends Ml_Controller_Action
 {
     use Ml_Controller_People;
 
+    public function pictureAddAction()
+    {
+        $this->_helper->layout->disableLayout();
+        $this->_helper->viewRenderer->setNoRender();
+
+        $picture =  $this->_sc->get("picture");
+        /** @var $picture \Ml_Model_Picture() */
+
+        $posts =  $this->_sc->get("posts");
+        /** @var $posts \Ml_Model_Posts() */
+
+        $post = $this->_post;
+        $userInfo = $this->_userInfo;
+
+        $numberOfPictures = count($post["pictures"]);
+
+        if ($numberOfPictures >= $posts->getMaxPicturesLimit()) {
+            $this->getResponse()->setHttpResponseCode(403);
+            $this->_helper->json(["error" => "too-many-pictures"]);
+            return;
+        }
+
+        $form = new Ml_Form_PicturePost();
+
+        if (! $this->getRequest()->isPost() || ! $form->isValid($this->getRequest()->getPost())) {
+            $this->getResponse()->setHttpResponseCode(404);
+            $this->_helper->json(["error" => ["validate" => $form->getErrors()]]);
+            return;
+        }
+
+        if ($form->Filedata->isUploaded()) {
+            $fileInfo = $form->Filedata->getFileInfo();
+
+            $id = $userInfo['id'] . "-p-" . $post["id"] . "-" . mt_rand();
+
+            $pictureInfo = $picture->create($fileInfo['Filedata']['tmp_name'], $id);
+
+            if (is_array($pictureInfo)) {
+                $z = $posts->addPicture($pictureInfo, $post["id"]);
+
+                $this->_helper->json($pictureInfo);
+            } else {
+                $this->getResponse()->setHttpResponseCode(404);
+                $this->_helper->json(["error" => "not-uploaded"]);
+                return;
+            }
+        }
+    }
+
 }
