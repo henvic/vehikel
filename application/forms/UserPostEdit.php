517b077bd1984c8741311f28824464d7c93b0811
Henrique Vicente <henriquevicente@gmail.com>
2012-12-10 21:55:09 -0300

    add the UserPostEdit form
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

---
+++ application/forms/UserPostEdit.php
@@ -0,0 +1,227 @@
+<?php
+class Ml_Form_UserPostEdit extends Twitter_Bootstrap_Form_Horizontal
+{
+    protected $_equipment;
+
+    protected $_username;
+    protected $_postId;
+
+    public function __construct($options = null, Zend_Translate $translate, array $equipment, $username, $postId)
+    {
+        $this->_equipment = $equipment;
+
+        $this->_username = $username;
+        $this->_postId = $postId;
+
+        $this->setTranslator($translate);
+
+        return parent::__construct($options);
+    }
+
+    public function init()
+    {
+        $this->setMethod('post');
+        $this->setAction($this->getView()->url(
+            array("username" => $this->_username, "post_id" => $this->_postId), "user_post_edit")
+        );
+
+        $this->addElementPrefixPath('Ml_Validate', 'Ml/Validate/',
+            Zend_Form_Element::VALIDATE);
+        $this->addElementPrefixPath('Ml_Filter', 'Ml/Filter/',
+            Zend_Form_Element::FILTER);
+        $this->addPrefixPath("Ml_Form", "Ml/Form");
+
+        $this->addElement('text', 'make', array(
+            'label' => 'Marca',
+            'required' => false,
+            'filters' => array('StringTrim'),
+            'validators' => array(
+                array('validator' =>
+                'StringLength', 'options' => array(1, 30))
+            )
+        ));
+
+        $this->addElement('text', 'model', array(
+            'label' => 'Modelo',
+            'required' => false,
+            'filters' => array('StringTrim'),
+            'validators' => array(
+                array('validator' =>
+                'StringLength', 'options' => array(1, 30))
+            )
+        ));
+
+        $this->addElement('text', 'name', array(
+            'label'      => 'Nome (anúncio)',
+            'required'   => false,
+            'filters'    => array('StringTrim'),
+            'validators' => array(
+                array('validator' =>
+                'StringLength', 'options' => array(1, 30))
+            ),
+            'class' => 'input-xlarge'
+        ));
+
+        $this->addElement('text', 'price', array(
+            'label'      => 'Preço',
+            'prepend' => 'R$',
+            'placeholder' => '00.000,00',
+            'required'   => false,
+            'filters'    => array('StringTrim'),
+            'validators' => array(
+                array('validator' =>
+                'StringLength', 'options' => array(1, 10)),
+            ),
+            'class' => 'input-small',
+            'maxlength' => 10
+        ));
+
+        $this->getElement("price")->addFilter(new Ml_Filter_CurrencyBr());
+        $this->getElement("price")->addValidator(new Ml_Validate_CurrencyBr());
+
+        $thisYear = gmdate("Y");
+        $years = ["" => "-"];
+        for ($yearCount = $thisYear; $yearCount >= $thisYear - 100; $yearCount--) {
+            $years[$yearCount] = $yearCount;
+        }
+
+        $this->addElement('select', 'build_year', array(
+            'label'      => 'Ano de fabricação',
+            'required'   => false,
+            'multiOptions' => $years,
+            'class' => 'input-small'
+        ));
+
+        $years = ["" => "-"];
+        for ($yearCount = $thisYear + 1; $yearCount >= $thisYear - 100; $yearCount--) {
+            $years[$yearCount] = $yearCount;
+        }
+
+        $this->addElement('select', 'model_year', array(
+            'label'      => 'Ano do modelo',
+            'required'   => false,
+            'multiOptions' => $years,
+            'class' => 'input-small'
+        ));
+
+        $engineCcs = ["" => "-"];
+
+        for ($engineCc = 0.8; $engineCc <= 5.8; $engineCc += 0.1) {
+            $engineCcFormated = number_format($engineCc, 1);
+            $engineCcs[$engineCcFormated] = $engineCcFormated;
+        }
+
+        $this->addElement('select', 'engine', array(
+            'label' => 'Motor',
+            'required' => false,
+            'multiOptions' => ["Motor" => $engineCcs],
+            'id' => 'post-product-engine',
+            'class' => 'input-mini'
+        ));
+
+        $this->addElement('select', 'traction', array(
+                'required' => false,
+                'label'    => 'Tração',
+                'multiOptions' => array(
+                    "" => "-",
+                    "front" => "Frontal",
+                    "rear" => "Traseira",
+                    "4x4" => "4x4"
+                )
+            )
+        );
+
+        $this->addElement(
+            'select',
+            'transmission',
+            array(
+                'required' => false,
+                'label'    => 'Câmbio',
+                'multiOptions' => array(
+                    "" => "-",
+                    "manual" => "Manual",
+                    "automatic" => "Automático"
+                )
+            )
+        );
+
+        $this->addElement('select', 'fuel', array(
+                'required' => false,
+                'label'    => 'Combustível',
+                'multiOptions' => array(
+                    "" => "-",
+                    "flex" => "Flexível",
+                    "gasoline" => "Gasolina",
+                    "ethanol" => "Etanol",
+                    "diesel" => "Diesel",
+                    "other" => "Outro"
+                )
+            )
+        );
+
+        $this->addElement('number', 'km', array(
+            'label'      => 'Quilometragem',
+            'required'   => false,
+            'class' => 'input-mini no-spin-button',
+            'min' => '0',
+            'max' => '9000000',
+            'maxlength' => 7,
+            'append' => 'km'
+        ));
+
+        $this->getElement("km")->addFilter(new Zend_Filter_Digits());
+
+        $this->getElement("km")->addValidator(new Zend_Validate_Int());
+
+        $this->addElement(
+            'checkbox',
+            'armor',
+            array(
+                'label' => 'Blindado',
+            )
+        );
+
+        $equipment = new Zend_Form_Element_MultiCheckbox('equipment');
+        $equipment->setLabel('Acessórios principais');
+
+        foreach ($this->_equipment as $eachEquipment) {
+            $equipment->addMultiOption($eachEquipment, "equipment_" . $eachEquipment);
+        }
+
+        $this->addElement($equipment);
+
+        $this->addElement('textarea', 'description', array(
+            'label'      => 'Descrição',
+            'description' =>
+            '<small><button class="btn btn-mini btn-link html-formatting-popover">HTML formatting</button></small>',
+            'required'   => false,
+            'filters'    => array('StringTrim'),
+            'validators' => array(
+                array('validator' =>
+                'StringLength', 'options' => array(0, 4096)),
+            ),
+            'rows' => 5
+        ));
+
+        $this->getElement('description')->getDecorator('description')->setEscape(false);
+
+        $this->addElement('submit', 'submit', array(
+            'label'    => 'Salvar',
+            'class' => 'btn btn-primary'
+        ));
+
+        $this->addElement(Ml_Model_MagicCookies::formElement());
+
+        $this->addDisplayGroup(
+            array('submit', 'reset'),
+            'actions',
+            array(
+                'disableLoadDefaultDecorators' => true,
+                'decorators' => array('Actions')
+            )
+        );
+
+        $this->setAttrib('class', 'form-stacked');
+        $this->setAttrib('id', 'post-manager');
+    }
+}
