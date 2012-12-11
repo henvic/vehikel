6b4b0a1ffeefa61331a73c611c79c5e0f7808890
Henrique Vicente <henriquevicente@gmail.com>
2012-12-10 22:36:01 -0300

    improve post.js
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- static/javascripts/views/user/post.js
+++ static/javascripts/views/user/post.js
@@ -4,46 +4,67 @@
 define(['jquery'], function ($) {
     "use strict";
 
-    var postInfoTabsLinks = $('#post-info-tabs a');
+    if (AppParams.postEditable === true) {
+        require(["views/user/post-manager"], function () {
+        });
+    }
 
-    postInfoTabsLinks.click(function (e) {
+    var $postInfoTabsLinks = $('#post-info-tabs a');
+
+    $postInfoTabsLinks.click(function (e) {
         e.preventDefault();
         $(this).tab('show');
     });
 
-    var postContactDiv = $('#post-contact');
+    var $postContact = $('#post-contact');
 
-    postContactDiv.on("submit", function (e) {
+    $postContact.on("submit", function (e) {
         var target = $(e.target);
         if (target.is('form')) {
             e.preventDefault();
 
-            var submit = $('input[type="submit"]', postContactDiv);
+            var $submit = $('input[type="submit"]', $postContact);
 
-            submit.data('loading-text', 'Enviando...').button('loading');
+            $submit.data('loading-text', 'Enviando...').button('loading');
 
             $.ajax({
                 type: 'POST',
                 data: target.serialize(),
                 success: function (result) {
-                    postContactDiv.html(result);
+                    $postContact.html(result);
                 },
                 complete: function (result) {
-                    submit.button('reset');
+                    $submit.button('reset');
                 }
             });
         }
     });
 
-    postContactDiv.on("click", function (e) {
+    $postContact.on("click", function (e) {
         var target = $(e.target);
         if (target.is('.close')) {
             $.ajax({
                 type: 'GET',
                 success: function (result) {
-                    postContactDiv.html(result);
+                    $postContact.html(result);
                 }
             });
+        }
+    });
+
+    var $postPicturesThumbnails = $("#post-pictures-thumbnails");
+    var $postPicturesCarousel = $("#post-pictures-carousel");
+
+    $postPicturesCarousel.carousel({
+        interval: 0
+    });
+
+    $postPicturesThumbnails.on("click", function (e) {
+        var $target = $(e.target);
+        var $closestLi = $target.closest("li");
+
+        if ($closestLi.hasClass("post-picture") && ! $target.hasClass("thumbnail-remove-picture")) {
+            $postPicturesCarousel.carousel($closestLi.index());
         }
     });
 });
