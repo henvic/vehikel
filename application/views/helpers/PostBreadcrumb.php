35dae545a7ee8c50f73555be391999b92f68602f
Henrique Vicente <henriquevicente@gmail.com>
2012-12-10 22:05:49 -0300

    add id to post breadcrumb
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- application/views/helpers/PostBreadcrumb.php
+++ application/views/helpers/PostBreadcrumb.php
@@ -3,7 +3,7 @@
 class Ml_View_Helper_PostBreadcrumb extends Zend_View_Helper_Abstract
 {
     /**
-     * @param array $paths array of arrays with name and link keys, the least one being the active
+     * @param array $paths array of arrays with id, name and link keys, the least one being the active
      * @return string ul breadcrumb
      */
     public function PostBreadCrumb($paths)
@@ -16,10 +16,11 @@ class Ml_View_Helper_PostBreadcrumb extends Zend_View_Helper_Abstract
             $path = $paths[$pos];
 
             if ($pos == $breadCrumbSize - 1) {
-                $content .= '<li class="active">' . $this->view->escape($path["name"]) . '</li>';
+                $content .= '<li id="' . $this->view->escape($path["id"]) . '" class="active">' .
+                    $this->view->escape($path["name"]) . '</li>';
             } else {
 
-                $content .= '<li><a href="' .
+                $content .= '<li id="' . $this->view->escape($path["id"]) . '"><a href="' .
                     $path["link"] .
                     '">' . $this->view->escape($path["name"]) .
                     '</a> <span class="divider">&gt;</span></li>'
