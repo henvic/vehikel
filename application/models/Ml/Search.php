10f39da5059d55493c123262c8c9c5b5e3f79820
Henrique Vicente <henriquevicente@gmail.com>
2013-03-05 00:09:12 -0300

    inject the application config to the Search model
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- application/models/Ml/Search.php
+++ application/models/Ml/Search.php
@@ -17,15 +17,22 @@ class Ml_Model_Search {
     protected $_cache;
 
     /**
+     * @var array
+     */
+    protected $_config;
+
+    /**
      * @param Ml_Model_People $people
      * @param Ml_Model_Posts $posts
      * @param Zend_Cache_Core $cache
+     * @param array $config Zend_Config array
      */
-    public function __construct($people, $posts, $cache)
+    public function __construct($people, $posts, $cache, $config)
     {
         $this->_people = $people;
         $this->_posts = $posts;
         $this->_cache = $cache;
+        $this->_config = $config;
     }
 
     public function syncUserById($id)
