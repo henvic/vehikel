a37d68f93c086d69207fa42923b7b0fb0771ac4e
Henrique Vicente <henriquevicente@gmail.com>
2013-04-06 00:28:41 -0300

    include the map modal code on the common plugin instead of the post js view helper
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- static/javascripts/plugins/common.js
+++ static/javascripts/plugins/common.js
@@ -1,11 +1,56 @@
 /*global define, window */
 /*jshint indent:4 */
 
-define(["AppParams", "jquery"],
-function (AppParams, $) {
+define(["AppParams", "jquery", "underscore", "text!templates/posts/map-modal.html"],
+function (AppParams, $, underscore, mapModalTemplate) {
     "use strict";
 
     $(window).ready(function () {
+        var mapClosure = (function () {
+            var $mapLink = $("#map-link");
+            var $mapModal = $("#map-modal");
+            var $mapModalInner;
+
+            var loadMapModal = function () {
+                var mapLink = $mapLink.attr("href");
+                var address = $mapLink.data("address");
+
+                var compiledMapModal = underscore.template(mapModalTemplate);
+                var mapModalHtml = compiledMapModal(
+                    {
+                        mapLink : mapLink,
+                        mapIframe : "https://maps.google.com/maps?f=q&source=s_q&hl=en&geocode=&q=" +
+                            encodeURIComponent(address) + "&ie=UTF8&z=15&t=m&iwloc=addr&output=embed"
+                    }
+                );
+
+                $mapModal.html(mapModalHtml);
+
+                $mapModalInner = $("#map-modal-inner");
+
+                $mapModalInner.modal({
+                    "backdrop" : false,
+                    "show" : false
+                });
+            };
+
+            $mapLink.on("click", function (e) {
+                var displayMapBool = (window.innerWidth >= 608);
+
+                if (! displayMapBool) {
+                    return;
+                }
+
+                e.preventDefault();
+
+                if (! $mapModalInner) {
+                    loadMapModal();
+                }
+
+                $mapModalInner.modal("show");
+            });
+        } ());
+
         var tooltipClosure = (function () {
             var tooltip = $("[rel=tooltip]");
             var likeTooltip = $("[data-rel=tooltip]");
