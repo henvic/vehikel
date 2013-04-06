10c3da71ff62cae1032255933e0059db0a109e33
Henrique Vicente <henriquevicente@gmail.com>
2013-04-06 15:40:59 -0300

    add width and height params settings for the picture view helper
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- application/views/helpers/Picture.php
+++ application/views/helpers/Picture.php
@@ -16,11 +16,16 @@ class Ml_View_Helper_Picture extends Zend_View_Helper_Abstract
         $this->_picture = $picture;
     }
 
-    public function picture($id, $secret, $format = "medium.jpg", $alt = 'picture')
+    public function picture($id, $secret, $format = "medium.jpg", $alt = 'picture', $width = false, $height = false)
     {
         $pictureLink = $this->_picture->getImageLink($id, $secret, $format);
 
-        $img = '<img src="' . $this->view->escape($pictureLink) . '" alt="' . $this->view->escape($alt) . '" />';
+        $img = '<img src="' .
+            $this->view->escape($pictureLink) .
+            '" alt="' . $this->view->escape($alt) . '"' .
+            ' width="' . $this->view->escape($width) . '"' .
+            ' height="' . $this->view->escape($height) . '"' .
+            ' />';
 
         return $img;
     }
