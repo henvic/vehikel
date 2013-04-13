b18d8348d4f473a28ad6f76be3e4d5e3dd8ef140
Henrique Vicente <henriquevicente@gmail.com>
2013-04-13 15:45:48 -0300

    add the drag & drop sorting functionality back to the new gallery management system
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- static/javascripts/views/user/post-manager.js
+++ static/javascripts/views/user/post-manager.js
@@ -761,5 +761,112 @@ define([
             }
         });
 
+        YUI().use('dd-constrain', 'dd-proxy', 'dd-drop', 'dd-delegate', 'dd-drop-plugin', function (Y) {
+            Y.DD.DDM.on('drop:over', function (e) {
+                var drag = e.drag.get('node'),
+                    drop = e.drop.get('node');
+
+                if (drop.get('tagName').toLowerCase() === 'li') {
+                    if (!goingUp) {
+                        drop = drop.get('nextSibling');
+                    }
+                    e.drop.get('node').get('parentNode').insertBefore(drag, drop);
+                    //Resize this nodes shim, so we can drop on it later.
+                    e.drop.sizeShim();
+                }
+            });
+
+            Y.DD.DDM.on('drag:drag', function (e) {
+                //Get the last y point
+                var y = e.target.lastXY[1];
+                //is it greater than the lastY var?
+                if (y < lastY) {
+                    //We are going up
+                    goingUp = true;
+                } else {
+                    //We are going down.
+                    goingUp = false;
+                }
+                //Cache for next check
+                lastY = y;
+            });
+
+            Y.DD.DDM.on('drag:start', function (e) {
+                var drag = e.target;
+                drag.get('node').setStyle('opacity', '.25');
+                drag.get('dragNode').set('innerHTML', drag.get('node').get('innerHTML'));
+                drag.get('dragNode').setStyles({
+                    opacity: '.5',
+                    borderColor: drag.get('node').getStyle('borderColor'),
+                    backgroundColor: drag.get('node').getStyle('backgroundColor')
+                });
+            });
+
+            Y.DD.DDM.on('drag:end', function (e) {
+                var drag = e.target;
+                drag.get('node').setStyles({
+                    visibility: '',
+                    opacity: '1'
+                });
+
+                var orderLis = Y.Node.all('#gallery-manager .pictures-thumbnails .picture');
+                var ids = [];
+                orderLis.each(function (v, k) {
+                    ids.push(v.getData("id"));
+                });
+
+                var post = $.ajax({
+                    url: AppParams.webroot + "/" + AppParams.postUsername + "/" + AppParams.postId + "/picture/sort",
+                    type: 'POST',
+                    dataType: 'json',
+                    data: {
+                        hash: AppParams.globalAuthHash,
+                        picture_id: ids
+                    }
+                });
+
+                post.done(function () {
+                    reloadImages();
+                });
+
+                post.fail(function () {
+                    reloadImages();
+                });
+            });
+
+            Y.DD.DDM.on('drag:drophit', function (e) {
+                var drop = e.drop.get('node');
+                var drag = e.drag.get('node');
+
+                //if we are not on an li, we must have been dropped on a ul
+                if (drop.get('tagName').toLowerCase() !== 'li') {
+                    if (!drop.contains(drag)) {
+                        drop.appendChild(drag);
+                    }
+                }
+            });
+
+            var goingUp = false, lastY = 0;
+
+            var del = new Y.DD.Delegate({
+                container: '.pictures-thumbnails',
+                nodes: '.picture',
+                target: {
+                    padding: '0 0 0 20'
+                }
+            });
+
+            del.dd.plug(Y.Plugin.DDProxy, {
+                moveOnEnd: false,
+                cloneNode: true
+            });
+
+            del.dd.plug(Y.Plugin.DDConstrained, {
+                constrain2node: '.pictures-thumbnails'
+            });
+
+            del.dd.plug(Y.Plugin.Drop);
+        });
+
     }
 );
