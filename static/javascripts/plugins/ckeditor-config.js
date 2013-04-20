89a6844952d8beaa7dce2725ad2095cd1e17659b
Henrique Vicente <henriquevicente@gmail.com>
2013-04-20 09:41:52 -0300

    add ckeditor-config file
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

---
+++ static/javascripts/plugins/ckeditor-config.js
@@ -0,0 +1,48 @@
+/*global define, CKEDITOR */
+/*jshint indent:4 */
+
+define(["ckeditor"], function () {
+    "use strict";
+
+    //open a simpler link dialog
+    CKEDITOR.on("dialogDefinition", function(ev) {
+        var dialogName = ev.data.name;
+        var dialogDefinition = ev.data.definition;
+
+        if (dialogName === "link") {
+            dialogDefinition.removeContents("target");
+            dialogDefinition.removeContents("advanced");
+
+            var infoTab = dialogDefinition.getContents("info");
+
+            infoTab.remove("linkType");
+            infoTab.remove("protocol");
+        }
+    });
+
+    var config = {};
+
+    config.customConfig = "";
+    config.language = "pt_BR";
+    config.height = "400px";
+    config.allowedContent = "p b i br u s; a[!href]";
+    config.entities = false;
+    config.basicEntities = true;
+    config.entities_greek = false;
+    config.entities_latin = false;
+    config.entities_additional = "";
+    config.htmlEncodeOutput = false;
+    config.removeButtons = "Anchor,Subscript,Superscript";
+
+    config.toolbar = [
+        { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', 'Underline', 'Strike', '-'] },
+        { name: 'links', items: [ 'Link', 'Unlink'] }
+    ];
+
+    config.toolbarGroups = [
+        { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+        { name: 'links' }
+    ];
+
+    return config;
+});
