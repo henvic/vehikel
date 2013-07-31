eeb966499fb948a011a4f45be34cdf715b67190c
Henrique Vicente <henriquevicente@gmail.com>
2013-07-31 00:07:50 -0300

    change the injected params of the Search service
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- application/models/Ml/Search.php
+++ application/models/Ml/Search.php
@@ -2,38 +2,16 @@
 
 class Ml_Model_Search {
     /**
-     * @var Ml_Model_People
-     */
-    protected $_people;
-
-    /**
-     * @var Ml_Model_Posts
-     */
-    protected $_posts;
-
-    /**
-     * @var Zend_Cache_Core
-     */
-    protected $_cache;
-
-    /**
      * @var array
      */
-    protected $_config;
+    protected $_searchConfig;
 
     /**
-     * @param Ml_Model_People $people
-     * @param Ml_Model_Posts $posts
-     * @param Zend_Cache_Core $cache
      * @param array $config Zend_Config array
      */
-    public function __construct($people, $posts, $cache, $config)
+    public function __construct($config)
     {
-        $this->_people = $people;
-        $this->_posts = $posts;
-        $this->_cache = $cache;
-        $this->_config = $config;
-    }
+        $this->_searchConfig = $config["services"]["search"];
     }
 
     {
