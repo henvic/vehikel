66e77f25f8bfdbab2c78eedbc102db4c032b14fb
Henrique Vicente <henriquevicente@gmail.com>
2013-07-31 00:06:15 -0300

    remove the Search's syncUser, syncUserById, and syncUserByUsername methods
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- application/models/Ml/Search.php
+++ application/models/Ml/Search.php
@@ -34,49 +34,17 @@ class Ml_Model_Search {
         $this->_cache = $cache;
         $this->_config = $config;
     }
-
-    public function syncUserById($id)
-    {
-        $userInfo = $this->_people->getById($id);
-
-        return $this->syncUser($userInfo);
     }
 
-    public function syncUserByUsername($username)
     {
-        $userInfo = $this->_people->getByUsername($username);
 
-        return $this->syncUser($userInfo);
-    }
 
-    public function syncUser($userInfo)
-    {
-        if (! $userInfo) {
-            return false;
         }
 
-        $syncUserInfoJob = $this->_people->syncSearch($userInfo);
 
-        if (! $syncUserInfoJob) {
-            error_log("Could not sync search db for user id " . (int) $userInfo["id"]);
+
+
         }
 
-        $postsIds = $this->_posts->getPostsIdsByUserId($userInfo["id"]);
-
-        $postsJobsSynced = 0;
-
-        foreach ($postsIds as $postId) {
-            $job = $this->_posts->syncSearch($postId, $userInfo);
-
-            if (! $job) {
-                error_log("Could not sync for post id " . (int) $postId);
-            } else {
-                $postsJobsSynced += 1;
-            }
-        }
-
-        unset($job);
-
-        return $postsJobsSynced;
     }
 }
