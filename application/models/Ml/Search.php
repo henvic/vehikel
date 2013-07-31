1e849be9c763eafe1b6693fde049f64c35d18464
Henrique Vicente <henriquevicente@gmail.com>
2013-07-31 00:08:08 -0300

    add the Search post method
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- application/models/Ml/Search.php
+++ application/models/Ml/Search.php
@@ -14,15 +14,39 @@ class Ml_Model_Search {
         $this->_searchConfig = $config["services"]["search"];
     }
 
+    /**
+     * @param string $index
+     * @param string $type
+     * @param int $id
+     * @param int $parentId
+     * @param array $data
+     * @return mixed int version on the search engine on success, false otherwise
+     */
+    public function post($index, $type, $id, $parentId = null, $data)
     {
+        $dataJson = json_encode($data);
 
+        $url = $this->_searchConfig["server"] . "/" . rawurlencode($index) . "/" . rawurlencode($type) . "/";
+        $url .= rawurlencode($id);
 
+        if ($parentId) {
+            $url .= "?parent=" . rawurlencode($parentId);
         }
 
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $dataJson);
 
+        $response = curl_exec($ch);
 
+        $result = json_decode($response, true);
 
+        if (json_last_error() === JSON_ERROR_NONE && isset($result["ok"]) && isset($result["_version"])) {
+            return $result["_version"];
         }
 
+        return false;
     }
 }
