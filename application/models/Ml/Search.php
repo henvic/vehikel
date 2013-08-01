594c5b36acf3d9bd6407279740312c285b5ade87
Henrique Vicente <henriquevicente@gmail.com>
2013-08-01 01:31:32 -0300

    add method callUpdatePublicProfileOnPosts on the Search model
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- application/models/Ml/Search.php
+++ application/models/Ml/Search.php
@@ -49,4 +49,44 @@ class Ml_Model_Search {
 
         return false;
     }
+
+    /**
+     * Call the ElasticSearch update_by_query plugin to update the posts with the new user info
+     * @param $userInfo
+     * @return number of updated fields on success, or false on failure
+     */
+    public function callUpdatePublicProfileOnPosts($publicUserInfo)
+    {
+        $data = [
+            "query" => [
+                "term" => [
+                    "user.id" => $publicUserInfo["id"]
+                ]
+            ],
+            "script" => "ctx._source.user = user",
+            "params" => [
+                "user" => $publicUserInfo
+            ]
+        ];
+
+        $dataJson = json_encode($data);
+
+        $url = $this->_searchConfig["server"] . "/posts/post/_update_by_query";
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $dataJson);
+
+        $response = curl_exec($ch);
+
+        $result = json_decode($response, true);
+
+        if (json_last_error() === JSON_ERROR_NONE && isset($result["ok"]) && isset($result["updated"])) {
+            return $result["updated"];
+        }
+
+        return false;
+    }
 }
