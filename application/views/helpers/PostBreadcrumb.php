dd16bef5dc8c79e6fda8e036fffa46c397ea2122
Henrique Vicente <henriquevicente@gmail.com>
2013-08-03 02:54:18 -0300

    make the breadcrumb appear muted when the post status isn't active, open #18
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- application/views/helpers/PostBreadcrumb.php
+++ application/views/helpers/PostBreadcrumb.php
@@ -4,15 +4,34 @@ class Ml_View_Helper_PostBreadcrumb extends Zend_View_Helper_Abstract
 {
     /**
      * @param array $paths array of arrays with id, name and link keys, the least one being the active
+     * @param bool $linksActive whether the links are enabled or not
      * @return string ul breadcrumb
      */
-    public function PostBreadCrumb($paths)
+    public function PostBreadCrumb($paths, $linksActive)
     {
-        $content = '<small class="post-breadcrumb">';
+        $isMuted = '';
+
+        if (! $linksActive) {
+            $isMuted = ' muted';
+        }
+
+        $content = '<small class="post-breadcrumb' . $isMuted . '">';
 
         $breadCrumbSize = count($paths);
 
-        $content .= '<span class="hidden-phone"><a href="' . $this->view->url(array(), "search") . '?q=*">Carros, motos e outros</a> &gt; </span>';
+        $content .= '<span class="hidden-phone">';
+
+        if ($linksActive) {
+            $content .= '<a href="' . $this->view->url(array(), "search") . '?q=*">';
+        }
+
+        $content .= 'Carros, motos e outros';
+
+        if ($linksActive) {
+            $content .= '</a>';
+        }
+
+        $content .= ' &gt; </span>';
 
         for ($pos = 0; $pos < $breadCrumbSize; $pos++) {
             $path = $paths[$pos];
@@ -22,10 +41,19 @@ class Ml_View_Helper_PostBreadcrumb extends Zend_View_Helper_Abstract
                     $this->view->escape($path["name"]) . '</span>';
             } else {
 
-                $content .= '<span id="' . $this->view->escape($path["id"]) . '"><a href="' .
-                    $path["link"] .
-                    '">' . $this->view->escape($path["name"]) .
-                    '</a> <span class="divider">&gt;</span> </span>'
+                $content .= '<span id="' . $this->view->escape($path["id"]) . '">';
+
+                if ($linksActive) {
+                    $content .= '<a href="' . $path["link"] . '">';
+                }
+
+                $content .= $this->view->escape($path["name"]);
+
+                if ($linksActive) {
+                    $content .= '</a>';
+                }
+
+                $content .= ' <span class="divider">&gt;</span> </span>'
                 ;
             }
         }
