dee647354af64c0df07ba73db26250966fe15cc4
Henrique Vicente <henriquevicente@gmail.com>
2013-08-30 22:32:25 -0300

    fix code style of the search worker.js
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- application/search/worker.js
+++ application/search/worker.js
@@ -1,21 +1,17 @@
 #!/usr/bin/env node
 
 /*jslint node: true */
-/*global require */
 
 module.exports = function () {
     "use strict";
 
-    var settings = require("./settings");
+    var settings = require("./settings"),
+        childProcess = require("child_process"),
+        nodeGearman = require("node-gearman"),
+        gearmanLibrary = require("./library/gearman")(nodeGearman),
+        gearman = gearmanLibrary.setUpGearman(settings.gearman.hostname, settings.gearman.port),
+        manager = require("./library/manager")(childProcess, gearman);
 
-    var childProcess = require("child_process");
-
-    var nodeGearman = require("node-gearman");
-
-    var gearmanLibrary = require("./library/gearman")(nodeGearman);
-    var gearman = gearmanLibrary.setUpGearman(settings.gearman.hostname, settings.gearman.port);
-
-    var manager = require("./library/manager")(childProcess, gearman);
     manager.setUpSyncUserProfileOnPostsFromUserWorker();
 };
 
