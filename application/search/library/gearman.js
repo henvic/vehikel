5d06803be8862aad417abc6d3ed1224908b9d4d5
Henrique Vicente <henriquevicente@gmail.com>
2013-08-31 02:12:19 -0300

    improve code style of gearman.js
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- application/search/library/gearman.js
+++ application/search/library/gearman.js
@@ -1,28 +1,28 @@
 /*jslint node: true */
 
 module.exports = function (Gearman) {
-    "use strict";
+    'use strict';
 
     exports.setUpGearman = function (hostname, port) {
 
         var gearman = new Gearman(hostname, port);
 
-        console.info("Listening to gearman on %s:%d", hostname, port);
+        console.info('Listening to gearman on %s:%d', hostname, port);
 
-        gearman.on("connect", function () {
-            console.info("Connected to Gearman!\n");
+        gearman.on('connect', function () {
+            console.info('Connected to Gearman!\n');
         });
 
-        gearman.on("idle", function () {
-            console.info("No jobs, resting!\n");
+        gearman.on('idle', function () {
+            console.info('No jobs, resting!\n');
         });
 
-        gearman.on("close", function () {
-            console.error("Gearman connection closed!\n");
+        gearman.on('close', function () {
+            console.error('Gearman connection closed!\n');
         });
 
-        gearman.on("error", function () {
-            console.error("Gearman error, disconnected!\n");
+        gearman.on('error', function () {
+            console.error('Gearman error, disconnected!\n');
         });
 
         return gearman;
