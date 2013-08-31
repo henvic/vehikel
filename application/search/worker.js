3eca0441240475e70f1bf42c7e814b0bc24d8097
Henrique Vicente <henriquevicente@gmail.com>
2013-08-31 02:14:00 -0300

    improve code style of worker.js
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- application/search/worker.js
+++ application/search/worker.js
@@ -3,14 +3,14 @@
 /*jslint node: true */
 
 module.exports = function () {
-    "use strict";
+    'use strict';
 
-    var settings = require("./settings"),
-        childProcess = require("child_process"),
-        nodeGearman = require("node-gearman"),
-        gearmanLibrary = require("./library/gearman")(nodeGearman),
+    var settings = require('./settings'),
+        childProcess = require('child_process'),
+        nodeGearman = require('node-gearman'),
+        gearmanLibrary = require('./library/gearman')(nodeGearman),
         gearman = gearmanLibrary.setUpGearman(settings.gearman.hostname, settings.gearman.port),
-        manager = require("./library/manager")(childProcess, gearman);
+        manager = require('./library/manager')(childProcess, gearman);
 
     manager.setUpSyncUserProfileOnPostsFromUserWorker();
 };
