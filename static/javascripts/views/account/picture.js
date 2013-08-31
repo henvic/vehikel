2ea3c4315e7f7e106bf92657729e9edbcc8078c6
Henrique Vicente <henriquevicente@gmail.com>
2013-08-31 01:57:24 -0300

    improve picture.js code style
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- static/javascripts/views/account/picture.js
+++ static/javascripts/views/account/picture.js
@@ -2,38 +2,38 @@
 /*jslint browser: true */
 
 define(['jquery'], function ($) {
-    "use strict";
+    'use strict';
 
     //jQuery doesn't copy e.dataTransfer natively
     var fileButton = document.getElementById('choose-image'),
         jFileButton = $('#choose-image'),
-        fileInput = document.getElementById("upload-image"),
-        imageElement = document.createElement("img"),
+        fileInput = document.getElementById('upload-image'),
+        imageElement = document.createElement('img'),
         createImage,
         previewImage;
 
     $(fileInput).hide();
 
-    fileInput.addEventListener("change", function () {
+    fileInput.addEventListener('change', function () {
         createImage(this.files[0]);
     }, false);
 
-    fileButton.addEventListener("click", function (e) {
+    fileButton.addEventListener('click', function (e) {
         $(fileInput).show().focus().click().hide();
         e.preventDefault();
     }, false);
 
-    fileButton.addEventListener("dragenter", function (e) {
+    fileButton.addEventListener('dragenter', function (e) {
         e.stopPropagation();
         e.preventDefault();
     }, false);
 
-    fileButton.addEventListener("dragover", function (e) {
+    fileButton.addEventListener('dragover', function (e) {
         e.stopPropagation();
         e.preventDefault();
     }, false);
 
-    fileButton.addEventListener("drop", function (e) {
+    fileButton.addEventListener('drop', function (e) {
         e.stopPropagation();
         e.preventDefault();
 
@@ -51,12 +51,12 @@ define(['jquery'], function ($) {
 
         setTimeout(function () {
             jFileButton.popover('hide');
-            imageElement.src = "";
+            imageElement.src = '';
         }, 2500);
     };
 
     createImage = function (file) {
-        if (typeof FileReader !== "function") {
+        if (typeof FileReader !== 'function') {
             return;
         }
 
