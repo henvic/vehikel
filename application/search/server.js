44b40efb48b9a4da594117310cc929d639623117
Henrique Vicente <henriquevicente@gmail.com>
2013-08-31 02:13:23 -0300

    improve code style of server.js
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- application/search/server.js
+++ application/search/server.js
@@ -3,26 +3,27 @@
 /*jslint node: true */
 
 module.exports = function () {
-    "use strict";
+    'use strict';
 
-    var settings = require("./settings"),
-        util = require("util"),
-        events = require("events"),
-        http = require("http"),
-        url = require("url"),
-        elastic = require("./library/elastic")(util, events, http, settings),
-        requests = require("./library/requests")(url, elastic),
+    var settings = require('./settings'),
+        util = require('util'),
+        events = require('events'),
+        http = require('http'),
+        url = require('url'),
+        elastic = require('./library/elastic')(util, events, http, settings),
+        requests = require('./library/requests')(url, elastic),
         server;
 
-    console.info("Loading search server\nListening on port %d (%s)", settings.port, settings.env);
+    console.info('Loading search server\n' +
+        'Listening on port %d (%s)', settings.port, settings.env);
 
     server = http.createServer();
 
     server.listen(settings.port);
 
-    server.on("request", requests.load);
+    server.on('request', requests.load);
 
-    server.addListener("request", requests.load);
+    server.addListener('request', requests.load);
 };
 
 module.exports();
