8caa2329d2cbe0feb2038785a923bcd6ba26d1a9
Henrique Vicente <henriquevicente@gmail.com>
2013-08-30 23:21:12 -0300

    fix code style of the common.js plugin
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- static/javascripts/plugins/common.js
+++ static/javascripts/plugins/common.js
@@ -1,82 +1,80 @@
-/*global define, window */
-/*jshint indent:4 */
+/*global define */
+/*jslint browser: true */
 
-define(["AppParams", "jquery", "underscore", "text!templates/posts/map-modal.html"],
-function (AppParams, $, underscore, mapModalTemplate) {
-    "use strict";
+define(["jquery", "underscore", "text!templates/posts/map-modal.html"],
+    function ($, underscore, mapModalTemplate) {
+        "use strict";
 
-    $(window).ready(function () {
-        var mapClosure = (function () {
-            var $mapLink = $("#map-link");
-            var $mapModal = $("#map-modal");
-            var $mapModalInner;
+        $(window).ready(function () {
+            (function () {
+                var $mapLink = $("#map-link"),
+                    $mapModal = $("#map-modal"),
+                    $mapModalInner,
+                    loadMapModal;
 
-            var loadMapModal = function () {
-                var mapLink = $mapLink.attr("href");
-                var address = $mapLink.data("address");
+                loadMapModal = function () {
+                    var mapLink = $mapLink.attr("href"),
+                        address = $mapLink.data("address"),
+                        compiledMapModal = underscore.template(mapModalTemplate),
+                        mapModalHtml = compiledMapModal(
+                            {
+                                mapLink : mapLink,
+                                mapIframe : "https://maps.google.com/maps?f=q&source=s_q&hl=en&geocode=&q=" +
+                                    encodeURIComponent(address) + "&ie=UTF8&z=15&t=m&iwloc=addr&output=embed"
+                            }
+                        );
 
-                var compiledMapModal = underscore.template(mapModalTemplate);
-                var mapModalHtml = compiledMapModal(
-                    {
-                        mapLink : mapLink,
-                        mapIframe : "https://maps.google.com/maps?f=q&source=s_q&hl=en&geocode=&q=" +
-                            encodeURIComponent(address) + "&ie=UTF8&z=15&t=m&iwloc=addr&output=embed"
+                    $mapModal.html(mapModalHtml);
+
+                    $mapModalInner = $("#map-modal-inner");
+
+                    $mapModalInner.modal({
+                        "backdrop" : false,
+                        "show" : false
+                    });
+                };
+
+                $mapLink.on("click", function (e) {
+                    var displayMapBool = (window.innerWidth >= 608);
+
+                    if (!displayMapBool) {
+                        return;
                     }
-                );
 
-                $mapModal.html(mapModalHtml);
+                    e.preventDefault();
 
-                $mapModalInner = $("#map-modal-inner");
+                    if (!$mapModalInner) {
+                        loadMapModal();
+                    }
 
-                $mapModalInner.modal({
-                    "backdrop" : false,
-                    "show" : false
+                    $mapModalInner.modal("show");
                 });
-            };
+            }());
 
-            $mapLink.on("click", function (e) {
-                var displayMapBool = (window.innerWidth >= 608);
+            (function () {
+                var tooltip = $("[rel=tooltip]"),
+                    likeTooltip = $("[data-rel=tooltip]");
 
-                if (! displayMapBool) {
-                    return;
+                if (tooltip.length) {
+                    tooltip.tooltip();
                 }
 
-                e.preventDefault();
-
-                if (! $mapModalInner) {
-                    loadMapModal();
+                if (likeTooltip.length) {
+                    likeTooltip.tooltip();
                 }
+            }());
 
-                $mapModalInner.modal("show");
+            var $siteMenu = $("#site-menu");
+
+            //avoid the site menu from closing when clicked
+            $siteMenu.on("click", function (e) {
+                e.stopPropagation();
             });
-        } ());
 
-        var tooltipClosure = (function () {
-            var tooltip = $("[rel=tooltip]");
-            var likeTooltip = $("[data-rel=tooltip]");
-
-            if (tooltip.length) {
-                tooltip.tooltip();
-            }
-
-            if (likeTooltip.length) {
-                likeTooltip.tooltip();
-            }
-        } ());
-
-        var $siteMenu = $("#site-menu");
-
-        //avoid the site menu from closing when clicked
-        $siteMenu.on("click", function(e) {
-            e.stopPropagation();
+            $("a[rel=\"external\"]")
+                .click(function () {
+                    window.open($(this).attr("href"));
+                    return false;
+                });
         });
-
-        $("a[rel=\"external\"]")
-           .click(function () {
-                window.open($(this).attr("href"));
-                return false;
-            });
     });
-
-    return function () {};
-});
