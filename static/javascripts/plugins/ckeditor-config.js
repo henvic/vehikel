8bc7d575b78d5e604b516e82aabe2df6b5ff81b6
Henrique Vicente <henriquevicente@gmail.com>
2013-08-31 01:56:47 -0300

    improve ckeditor-config.js code style
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- static/javascripts/plugins/ckeditor-config.js
+++ static/javascripts/plugins/ckeditor-config.js
@@ -1,60 +1,60 @@
 /*global define, CKEDITOR */
 /*jslint browser: true */
 
-define(["ckeditor"], function () {
-    "use strict";
+define(['ckeditor'], function () {
+    'use strict';
 
     //open a simpler link dialog
-    CKEDITOR.on("dialogDefinition", function (ev) {
+    CKEDITOR.on('dialogDefinition', function (ev) {
         var dialogName = ev.data.name,
             dialogDefinition = ev.data.definition,
             infoTab;
 
-        if (dialogName === "link") {
-            dialogDefinition.removeContents("target");
-            dialogDefinition.removeContents("advanced");
+        if (dialogName === 'link') {
+            dialogDefinition.removeContents('target');
+            dialogDefinition.removeContents('advanced');
 
-            infoTab = dialogDefinition.getContents("info");
+            infoTab = dialogDefinition.getContents('info');
 
-            infoTab.remove("linkType");
-            infoTab.remove("protocol");
+            infoTab.remove('linkType');
+            infoTab.remove('protocol');
         }
     });
 
     var config = {};
 
-    config.customConfig = "";
-    config.language = "pt_BR";
-    config.height = "400px";
-    config.allowedContent = "p b i br u s; a[!href]";
+    config.customConfig = '';
+    config.language = 'pt_BR';
+    config.height = '400px';
+    config.allowedContent = 'p b i br u s; a[!href]';
     config.entities = false;
     config.basicEntities = true;
     config.entities_greek = false;
     config.entities_latin = false;
-    config.entities_additional = "";
+    config.entities_additional = '';
     config.htmlEncodeOutput = false;
-    config.removeButtons = "Anchor,Subscript,Superscript";
+    config.removeButtons = 'Anchor,Subscript,Superscript';
 
     config.toolbar = [
         {
             name: 'basicstyles',
-            groups: ['basicstyles', 'cleanup' ],
+            groups: ['basicstyles', 'cleanup'],
             items: ['Bold', 'Italic', 'Underline', 'Strike', '-']
         },
         {
             name: 'links',
-            items: [ 'Link', 'Unlink']
+            items: ['Link', 'Unlink']
         },
         {
             name: 'clipboard',
-            items : [ 'Undo', 'Redo' ]
+            items: ['Undo', 'Redo']
         }
     ];
 
     config.toolbarGroups = [
         {
             name: 'basicstyles',
-            groups: [ 'basicstyles', 'cleanup' ]
+            groups: ['basicstyles', 'cleanup']
         },
         {
             name: 'links'
