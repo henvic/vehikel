9738fe4a5b438bd2e6a341a0c37814017fae5b40
Henrique Vicente <henriquevicente@gmail.com>
2013-11-26 02:24:33 -0300

    Adding the advanced search intelligence & improving the form
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- static/javascripts/views/index/index.js
+++ static/javascripts/views/index/index.js
@@ -1,12 +1,17 @@
 /*global define, require */
 /*jslint browser: true */
 
-define(['jquery'], function ($) {
+define(['jquery', 'models/vehicles'], function ($, vehiclesModel) {
     'use strict';
 
     var $indexContent = $('#index-content'),
         $searchText = $('#search-text'),
+        $advancedSearchType = $('#advanced-search-type'),
+        $advancedSearchMake = $('#advanced-search-make'),
+        $advancedSearchModel = $('#advanced-search-model'),
         closeIndexContent;
+
+    vehiclesModel.setUp($advancedSearchType, $advancedSearchMake, $advancedSearchModel);
 
     closeIndexContent = function () {
         if ($indexContent === undefined) {
